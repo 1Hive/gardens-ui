4d3e6464618e5602d3f64ec94fde6138352ee043
Fabrizio Vigevani <fabriziovigevani@gmail.com>
2020-06-22 04:19:20 -0300

    Implement app state provider using connect

--- src/app-logic.js
+++ src/app-logic.js
@@ -1,44 +1,48 @@
-import { useState, useMemo } from 'react'
+import { useCallback, useMemo, useState } from 'react'
 import BigNumber from './lib/bigNumber'
-import { useAppState } from '@aragon/api-react'
 import { toDecimals } from './lib/math-utils'
 import { toHex } from 'web3-utils'
+import { useWallet } from './providers/Wallet'
 import { useProposals } from './hooks/useProposals'
-import { useAppData } from './hooks/useAppData'
-import { useWallet } from 'use-wallet'
+import { useAppState } from './providers/AppState'
 
 // Handles the main logic of the app.
 export default function useAppLogic() {
-  const { connectedAccount } = useWallet()
+  const { connectedAccount, ethers } = useWallet()
 
-  const { stakeToken, requestToken, isSyncing } = useAppState()
-  const [proposals, blockHasLoaded] = useProposals()
+  const {
+    organization,
+    convictionApp,
+    isLoading,
+    requestToken,
+    stakeToken,
+  } = useAppState()
+  const [proposals] = useProposals()
   const [proposalPanel, setProposalPanel] = useState(false)
 
-  const { organization, convictionApp } = useAppData()
+  const onNewProposal = useCallback(
+    async ({ title, link, amount, beneficiary }) => {
+      const { decimals } = requestToken
+      const decimalAmount = toDecimals(amount.trim(), decimals).toString()
 
-  const onProposalSubmit = async ({ title, link, amount, beneficiary }) => {
-    const decimals = parseInt(requestToken.decimals)
-    const decimalAmount = toDecimals(amount.trim(), decimals).toString()
+      const intent = organization.appIntent(
+        convictionApp.appAddress,
+        'addProposal',
+        [title, toHex(link), decimalAmount, beneficiary]
+      )
 
-    const intent = organization.appIntent(
-      convictionApp.appAddress,
-      'addProposal',
-      [title, toHex(link), decimalAmount, beneficiary]
-    )
+      const txPath = await intent.paths(connectedAccount)
 
-    // const intent = org.appIntent(finance.address, 'newImmediatePayment', [ ethers.constants.AddressZero, ACCOUNT, ethers.utils.parseEther('1'), 'Tests Payment', ])
+      const { to, data } = txPath.transactions[0]
+      ethers.getSigner().sendTransaction({ data, to })
 
-    const txPath = await intent.paths(connectedAccount)
-
-    console.log('txPath ', txPath)
-
-    // api.addProposal(title, toHex(link), decimalAmount, beneficiary).toPromise()
-    setProposalPanel(false)
-  }
+      setProposalPanel(false)
+    },
+    [connectedAccount, convictionApp, ethers, requestToken, organization]
+  )
 
   const { myStakes, totalActiveTokens } = useMemo(() => {
-    if (!connectedAccount || !stakeToken.tokenDecimals || !proposals) {
+    if (!connectedAccount || !stakeToken || !proposals) {
       return { myStakes: [], totalActiveTokens: new BigNumber('0') }
     }
 
@@ -69,12 +73,12 @@ export default function useAppLogic() {
       },
       { myStakes: [], totalActiveTokens: new BigNumber('0') }
     )
-  }, [proposals, connectedAccount, stakeToken.tokenDecimals])
+  }, [proposals, connectedAccount, stakeToken])
 
   return {
     proposals,
-    isSyncing: isSyncing || !blockHasLoaded,
-    onProposalSubmit,
+    isLoading: isLoading,
+    onNewProposal,
     proposalPanel,
     setProposalPanel,
     myStakes,
