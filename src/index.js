2b63f939bcc63dd17b6e7d36f597c1803c823933
Fabrizio Vigevani <fabriziovigevani@gmail.com>
2020-06-25 01:11:10 -0300

    Do various improvements

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
+import { Main } from '@aragon/ui'
+import theme from './theme-1hive'
 
 import App from './App'
-import { Main } from '@aragon/ui'
-import theme from './theme-conviction'
+import MainView from './components/MainView'
 import { WalletProvider } from './providers/Wallet'
 import { AppStateProvider } from './providers/AppState'
 
 ReactDOM.render(
   <WalletProvider>
     <AppStateProvider>
-      <Main theme={theme} assetsUrl="/aragon-ui/">
+      <Main theme={theme} assetsUrl="/aragon-ui/" layout={false}>
         <BrowserRouter>
-          <App />
+          <MainView>
+            <App />
+          </MainView>
         </BrowserRouter>
       </Main>
     </AppStateProvider>
---
+++ src/styles/breakpoints.js
@@ -0,0 +1,9 @@
+import { GU } from '@aragon/ui'
+
+// These breakpoints values represent minimum screen sizes.
+export const BREAKPOINTS = {
+  min: 360,
+  small: 680,
+  medium: 100 * GU,
+  large: 170 * GU,
+}
