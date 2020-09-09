8aeaf5dc4ef53486b7e609abdbcb8a07652cb4fe
Fabrizio Vigevani <fabriziovigevani@gmail.com>
2020-09-08 21:06:00 -0300

    Balance module: Show total balance

--- src/components/BalanceModule.js
+++ src/components/BalanceModule.js
@@ -60,7 +60,7 @@ function BalanceModule() {
           </div>
           <div>
             <span>
-              {formatTokenAmount(myActiveTokens, stakeToken.decimals)}
+              {formatTokenAmount(accountBalance, stakeToken.decimals)}
             </span>{' '}
             <span
               css={`
