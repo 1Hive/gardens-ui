936a2cdd0c431eda0183f4831a7a50009674d47d
Fabian Molina <fmolina@tangocode.com>
2020-09-27 22:33:38 -0300

    UI Modal for Network Error

--- src/components/NetworkErrorModal.js
+++ src/components/NetworkErrorModal.js
@@ -1,21 +1,41 @@
 import React from 'react'
-import { GU, Modal } from '@1hive/1hive-ui'
-import flower_error from '../assets/flower_error.svg'
+import { GU, Modal, textStyle, useViewport } from '@1hive/1hive-ui'
+import flowerError from '../assets/flowerError.svg'
 
-function NetworkErrorModal({visible}) {
-    <Modal
-        padding={8*GU}
-    >
-        <div css={`
+function NetworkErrorModal({ visible }) {
+    const { below } = useViewport()
+    const compactMode = below('medium')
+
+    return (
+        <Modal padding={7 * GU} visible={visible} width={compactMode ? '200px' : '400px'}>
+        <div
+            css={`
             display: flex;
             flex-direction: column;
-        `}>
-            <img src={flower_error} alt=""/>
-            <h3 css={`
-                ${textStyle("title2")}
-            `}>Something went wrong</h3>
+            align-items: center;
+            `}
+        >
+            <img src={flowerError} alt="" height="88" width="71"/>
+            <h3
+            css={`
+                ${textStyle('title3')}
+                line-height: 40px;
+                margin-top: 24px;
+                margin-bottom: 8px;
+                text-align:center;
+            `}>
+            Something went wrong
+            </h3>
+            <h4
+                css={`
+                    ${textStyle("body4")}
+                    text-align:center;
+                `}>
+                    An error has occurred with the network connection.
+                </h4>
         </div>
-    </Modal>
+        </Modal>
+    )
 }
 
-export default NetworkErrorModal;
+export default NetworkErrorModal
