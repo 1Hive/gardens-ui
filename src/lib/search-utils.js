771e32b2b9ba782e7743d00df2a1029001f6354e
anthonyoliai <anthonyoliai@gmail.com>
2020-09-30 13:21:19 +0200

    Added '\'

--- src/lib/search-utils.js
+++ src/lib/search-utils.js
@@ -7,7 +7,7 @@ export function checkInitialLetters(text, searchTerm) {
   const str = text.substring(0, NUMBER_CHARACTERS)
 
   // Transform regex special characters into a '_' for easy reference.
-  searchTerm = searchTerm.replace(/[\*\[\(\)+\]]/g,' ');
+  searchTerm = searchTerm.replace(/[\*\[\(\)\\+\]]/g,' ');
   
   const pattern = searchTerm
     .split('')
