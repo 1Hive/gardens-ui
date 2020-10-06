00f1ded711b27d0127418461f9106fe612294099
Rodrigo Perez <rodrigopi1989@gmail.com>
2020-10-06 12:20:18 -0300

    endpoints

--- src/endpoints.js
+++ src/endpoints.js
@@ -1,5 +1,9 @@
 import { isLocalOrUnknownNetwork } from './lib/web3-utils'
 
+export const BRIGHT_ID_ENDPOINT_V5 = 'https://app.brightid.org/node/v5'
+export const BRIGHTID_VERIFICATION_ENDPOINT = `${BRIGHT_ID_ENDPOINT_V5}/verifications`
+export const BRIGHTID_1HIVE_INFO_ENDPOINT = `${BRIGHT_ID_ENDPOINT_V5}/apps/1hive`
+
 // IPFS endpoint
 export const IPFS_ENDPOINT = {
   read: isLocalOrUnknownNetwork()
