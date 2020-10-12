07b8200b76b5de79ae5eb127dc07bf4b8ae21410
Fabrizio Vigevani <fabriziovigevani@gmail.com>
2020-10-12 19:27:28 -0300

    Implement proposal logic hook

---
+++ src/logic/proposal-logic.js
@@ -0,0 +1,34 @@
+import useActions from '../hooks/useActions'
+import { useAppState } from '../providers/AppState'
+import usePanelState from '../hooks/usePanelState'
+import { useProposal } from '../hooks/useProposals'
+
+export default function useProposalLogic(match) {
+  const { params, path } = match
+  const { id: proposalId } = params
+
+  const {
+    config,
+    isLoading,
+    permissions,
+    requestToken,
+    vaultBalance,
+  } = useAppState()
+  const appAddress = path.includes('vote')
+    ? config?.voting.id
+    : config?.conviction.id
+
+  const panelState = usePanelState()
+  const actions = useActions(panelState.requestClose)
+  const [proposal, blockHasLoaded] = useProposal(proposalId, appAddress)
+
+  return {
+    actions,
+    isLoading: isLoading || !blockHasLoaded,
+    panelState,
+    permissions,
+    proposal,
+    requestToken,
+    vaultBalance,
+  }
+}
