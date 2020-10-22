b7fde76ebbc873fd218d54021fbb6174cd86f55d
Fabrizio Vigevani <fabriziovigevani@gmail.com>
2020-10-21 21:01:34 -0300

    Use appAddres for template creation

--- connector/subgraph/src/kernel.ts
+++ connector/subgraph/src/kernel.ts
@@ -3,14 +3,12 @@ import { NewAppProxy as NewAppProxyEvent } from '../generated/Kernel/Kernel'
 import { loadConvictionConfig, loadVotingConfig } from './helpers'
 
 const DANDELION_VOTING_APP_IDS: string[] = [
-  '0x2d7442e1c4cb7a7013aecc419f938bdfa55ad32d90002fb92ee5969e27b2bf07', // dandelion-voting.aragonpm.eth
-  '0x40a80c4b4050993512df39c802adec62dafeb1f0586cc15f4d34bda9c47ba468',  // gardens-dandelion-voting.open.aragonpm.eth 
-  '0x82d8d87cd48f4b245d5c8c1c98c5d527dc7b7e166f62471569fb8b1061900644'  // dandelion-voting.1hive.aragonpm.eth
+  // '0x2d7442e1c4cb7a7013aecc419f938bdfa55ad32d90002fb92ee5969e27b2bf07', // dandelion-voting.aragonpm.eth
+  // '0x40a80c4b4050993512df39c802adec62dafeb1f0586cc15f4d34bda9c47ba468',  // gardens-dandelion-voting.open.aragonpm.eth 
+  '0x82d8d87cd48f4b245d5c8c1c98c5d527dc7b7e166f62471569fb8b1061900644',  // dandelion-voting.1hive.aragonpm.eth
+  // '0xcb3a8075b5aedae361e3d76cfc928af1dd44a9308cdf86f8f5f6109a8f954420' // dandelion-voting-beta.aragonpm.eth
 ]
-const CONIVCTION_VOTING_APP_IDS: string[] = [
-  '0xabb88ccde8e73f80a3f4a14ef4f6bbfcc19f172a073a5d4cace3af06a8f2a182', // conviction-beta.aragonpm.eth
-  '0xe4691f497f5e74daf61612cea2d5a540b095805872218eaa9108aa5fd76779a2'  // conviction-beta.open.aragonpm.eth
-]
+const CONIVCTION_VOTING_ADDRESS: Address = Address.fromString('0x7f67f5de7b815c5709be70f5eea627de43bb1b0c')
 
 export function handleNewAppProxy(event: NewAppProxyEvent): void {
   processApp(event.address, event.params.proxy, event.params.appId.toHexString())
@@ -21,7 +19,7 @@ export function handleNewAppProxy(event: NewAppProxyEvent): void {
 
     if (DANDELION_VOTING_APP_IDS.includes(appId)) {
       template = 'DandelionVoting'
-    } else if (CONIVCTION_VOTING_APP_IDS.includes(appId)) {
+    } else if (CONIVCTION_VOTING_ADDRESS.equals(appAddress)) {
       template = 'ConvictionVoting'
     }
 
@@ -32,7 +30,7 @@ export function handleNewAppProxy(event: NewAppProxyEvent): void {
 }
 
 function onAppTemplateCreated(orgAddress: Address, appAddress: Address, appId: string): void {
-  if (CONIVCTION_VOTING_APP_IDS.includes(appId)) {
+  if (CONIVCTION_VOTING_ADDRESS.equals(appAddress)) {
     loadConvictionConfig(orgAddress, appAddress)
     return 
   }
