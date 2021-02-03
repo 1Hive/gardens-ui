b004782de1e7d5288ea216b8af585b4f8a4c9a4d
Fabian Molina <fmolina@tangocode.com>
2021-02-03 17:49:29 -0300

    Support proposal multimodal first draft

---
+++ src/components/ModalFlows/SupportProposal/SupportProposalScreens.js
@@ -0,0 +1,45 @@
+import React, { useMemo, useState, useCallback } from 'react'
+import PropTypes from 'prop-types'
+import ModalFlowBase from '../ModalFlowBase'
+import SignOverview from './SignOverview'
+import useActions from '../../../hooks/useActions'
+
+function SupportProposalScreens({ versionId }) {
+  const actions = useActions()
+  const [transactions, setTransactions] = useState([])
+
+  const getTransactions = useCallback(
+    async onComplete => {
+      await actions.agreementActions.signAgreement({ versionId }, intent => {
+        setTransactions(intent.transactions)
+        onComplete()
+      })
+    },
+    [actions, versionId]
+  )
+
+  const screens = useMemo(
+    () => [
+      {
+        title: 'Sign Agreement',
+        graphicHeader: true,
+        content: <SignOverview getTransactions={getTransactions} />,
+      },
+    ],
+    [getTransactions]
+  )
+  return (
+    <ModalFlowBase
+      frontLoad={false}
+      transactions={transactions}
+      transactionTitle="Sign Agreement"
+      screens={screens}
+    />
+  )
+}
+
+SupportProposalScreens.propTypes = {
+  versionId: PropTypes.string,
+}
+
+export default SupportProposalScreens
