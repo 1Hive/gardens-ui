25c8495b79a1b50bb2152059ea0bcebc7e69f0da
Fabro <fabriziovigevani@gmail.com>
2021-02-24 15:13:44 -0300

    Fix bug

--- src/components/ModalFlows/CreateProposalScreens/CreateProposalScreens.js
+++ src/components/ModalFlows/CreateProposalScreens/CreateProposalScreens.js
@@ -10,8 +10,8 @@ import { useStakingState } from '../../../providers/Staking'
 import { ZERO_ADDR } from '../../../constants'
 
 function CreateProposalScreens() {
-  const [agreement, loading] = useAgreement()
-  const { stakeManagement } = useStakingState()
+  const [agreement, agreementLoading] = useAgreement()
+  const { stakeManagement, loading: stakingLoading } = useStakingState()
   const [proposalData, setProposalData] = useState()
   const [transactions, setTransactions] = useState([])
   const { convictionActions } = useActions()
@@ -39,39 +39,42 @@ function CreateProposalScreens() {
   )
 
   const screens = useMemo(
-    () => [
-      {
-        title: 'Submit action requirements',
-        graphicHeader: false,
-        content: (
-          <CreateProposalRequirements
-            agreement={agreement}
-            availableStaked={stakeManagement.staking.available}
-          />
-        ),
-      },
-      {
-        title: 'Create post',
-        graphicHeader: true,
-        content: <AddProposal setProposalData={setProposalData} />,
-      },
-      {
-        title: 'Action fees',
-        graphicHeader: false,
-        content: (
-          <ActionFees
-            agreement={agreement}
-            onCreateTransaction={getTransactions}
-          />
-        ),
-      },
-    ],
-    [agreement, getTransactions, stakeManagement.staking.available]
+    () =>
+      stakingLoading
+        ? []
+        : [
+            {
+              title: 'Submit action requirements',
+              graphicHeader: false,
+              content: (
+                <CreateProposalRequirements
+                  agreement={agreement}
+                  availableStaked={stakeManagement.staking.available}
+                />
+              ),
+            },
+            {
+              title: 'Create post',
+              graphicHeader: true,
+              content: <AddProposal setProposalData={setProposalData} />,
+            },
+            {
+              title: 'Action fees',
+              graphicHeader: false,
+              content: (
+                <ActionFees
+                  agreement={agreement}
+                  onCreateTransaction={getTransactions}
+                />
+              ),
+            },
+          ],
+    [agreement, getTransactions, stakingLoading, stakeManagement]
   )
   return (
     <ModalFlowBase
       frontLoad
-      loading={loading}
+      loading={agreementLoading || stakingLoading}
       transactions={transactions}
       transactionTitle="Create transaction"
       screens={screens}
--- src/hooks/useDispute.js
+++ src/hooks/useDispute.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import { getNetwork } from '../networks'
-import arbitratorAbi from '../abi/arbitrator.json'
+import arbitratorAbi from '../abi/Arbitrator.json'
 import disputeManagerAbi from '../abi/DisputeManager.json'
 import { useContractReadOnly } from './useContract'
 import { DISPUTE_STATE_ADJUDICATING } from '../utils/dispute-utils'
