f5053c4a945b9f5ae8bc19288ab9ce73ae4e7a1a
Rodrigo Perez <rodrigopi1989@gmail.com>
2021-03-17 12:57:31 -0300

    new deployment data

--- connector/subgraph/src/kernel.ts
+++ connector/subgraph/src/kernel.ts
@@ -3,14 +3,14 @@ import { NewAppProxy as NewAppProxyEvent } from '../generated/Kernel/Kernel'
 import { loadConvictionConfig } from './helpers'
 
 const DISPUTABLE_VOTING_ADDRESS: Address = Address.fromString(
-  '0x05fb05d70315cde1e9c3a026e81055d6f2811766'
+  '0xaa2656f7955be77dfb26950434783ae1a489210e'
 )
 const CONIVCTION_VOTING_ADDRESS: Address = Address.fromString(
-  '0x16ac9a05d59fcbe45bdb1faa4870268a8bd8e7a7'
+  '0x0cc6f0962b415788a5dca32354b5da7dedf21399'
 )
 
 const AGREEMENT_ADDRESS: Address = Address.fromString(
-  '0xc08d59bfe2337d45fa5e77ad8d683fd5bdfcde95'
+  '0x15d99c0ba7cd951a9cadeb9bff4d603a1af23c3c'
 )
 
 function onAppTemplateCreated(
