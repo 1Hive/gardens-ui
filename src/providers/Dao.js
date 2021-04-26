086e61eb167436799a8167f2c43ca3c698a9c121
Rodrigo Perez <rodrigopi1989@gmail.com>
2021-04-25 21:17:48 -0300

    DAO Provider

---
+++ src/providers/Dao.js
@@ -0,0 +1,34 @@
+import React, { useContext } from 'react'
+import { addressesEqual } from '@1hive/1hive-ui'
+import { useLocation } from 'react-router-dom'
+import daoList from '@1hive/gardens-dao-list'
+import { isAddress } from '../utils/web3-utils'
+import { DAONotFound } from '../errors'
+
+const DAOContext = React.createContext()
+
+export function parsePath(pathname, search = '') {
+  const [, ...parts] = pathname.split('/')
+
+  return parts[0]
+}
+
+export function DAOProvider({ children }) {
+  const location = useLocation()
+
+  // note that i am calling this daoId because at some point we might want to also handle aragon id's and not just addresses
+  const firstParameter = parsePath(location.pathname)
+  const validAddress = isAddress(firstParameter)
+  const dao = daoList.daos.find(d => addressesEqual(firstParameter, d.address))
+
+  // We should bypass the error if the Gardens home page is invoked, right now we don't have one but probably will be the dao explorer
+  if ((!validAddress || !dao) && firstParameter !== 'home') {
+    throw new DAONotFound(firstParameter)
+  }
+
+  return <DAOContext.Provider value={dao}>{children}</DAOContext.Provider>
+}
+
+export function useDAO() {
+  return useContext(DAOContext)
+}
