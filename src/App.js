6562cbb47b66b3ebc52d977eaf48544b73e84b48
Rodrigo Perez <rodrigopi1989@gmail.com>
2021-04-25 21:18:38 -0300

    App routing

--- src/App.js
+++ src/App.js
@@ -4,34 +4,44 @@ import { Main } from '@1hive/1hive-ui'
 import MainView from './components/MainView'
 import Routes from './Routes'
 
+import GlobalErrorHandler from './GlobalErrorHandler'
 import { AgreementSubscriptionProvider } from './providers/AgreementSubscription'
 import { AppStateProvider } from './providers/AppState'
 import { ProfileProvider } from './providers/Profile'
+import { DAOProvider } from './providers/Dao'
 import { StakingProvider } from './providers/Staking'
 import { WalletProvider } from './providers/Wallet'
 import { ConnectProvider as Connect } from './providers/Connect'
 
 function App() {
   return (
-    <WalletProvider>
-      <Connect>
-        <ProfileProvider>
-          <AppStateProvider>
-            <StakingProvider>
-              <AgreementSubscriptionProvider>
-                <Main assetsUrl="/aragon-ui/" layout={false} scrollView={false}>
-                  <HashRouter>
-                    <MainView>
-                      <Routes />
-                    </MainView>
-                  </HashRouter>
-                </Main>
-              </AgreementSubscriptionProvider>
-            </StakingProvider>
-          </AppStateProvider>
-        </ProfileProvider>
-      </Connect>
-    </WalletProvider>
+    <GlobalErrorHandler>
+      <HashRouter>
+        <DAOProvider>
+          <WalletProvider>
+            <Connect>
+              <ProfileProvider>
+                <AppStateProvider>
+                  <StakingProvider>
+                    <AgreementSubscriptionProvider>
+                      <Main
+                        assetsUrl="/aragon-ui/"
+                        layout={false}
+                        scrollView={false}
+                      >
+                        <MainView>
+                          <Routes />
+                        </MainView>
+                      </Main>
+                    </AgreementSubscriptionProvider>
+                  </StakingProvider>
+                </AppStateProvider>
+              </ProfileProvider>
+            </Connect>
+          </WalletProvider>
+        </DAOProvider>
+      </HashRouter>
+    </GlobalErrorHandler>
   )
 }
 
--- src/Routes.js
+++ src/Routes.js
@@ -6,18 +6,22 @@ import Home from './screens/Home'
 import Profile from './screens/Profile'
 import ProposalLoader from './components/ProposalLoader'
 import StakeManagement from './components/Stake/StakeManagement'
+import { useDAO } from './providers/Dao'
 
 export default function Routes() {
+  const { address: daoAddress } = useDAO()
+
   return (
     <Switch>
-      <Redirect exact from="/" to="/home" />
-      <Route path="/home" component={Home} />
-      <Route exact path="/profile" component={Profile} />
-      <Route exact path="/proposal/:id" component={ProposalLoader} />
-      <Route exact path="/vote/:id" component={DecisionLoader} />
-      <Route exact path="/covenant" component={Agreement} />
-      <Route exact path="/collateral" component={StakeManagement} />
-      <Redirect to="/home" />
+      {/* Garden specific home page, we also need to have a rout for the main 1hive.org main page that will contain the onboarding/dao explorer */}
+      <Redirect exact from={`*/${daoAddress}/`} to={`${daoAddress}/home`} />
+      <Route path="*/home" component={Home} />
+      <Route exact path="*/profile" component={Profile} />
+      <Route exact path="*/proposal/:id" component={ProposalLoader} />
+      <Route exact path="*/vote/:id" component={DecisionLoader} />
+      <Route exact path="*/covenant" component={Agreement} />
+      <Route exact path="*/collateral" component={StakeManagement} />
+      {/* <Redirect to="/home" /> */}
     </Switch>
   )
 }
