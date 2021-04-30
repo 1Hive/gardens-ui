db3dca11c77785b078af99c43a01e51c35146ea8
Fabro <fabriziovigevani@gmail.com>
2021-04-30 17:06:00 -0300

    Replace honeypot connector for garden connector

--- src/networks.js
+++ src/networks.js
@@ -2,9 +2,6 @@ import { getNetworkType, isLocalOrUnknownNetwork } from './utils/web3-utils'
 import { getDefaultChain } from './local-settings'
 import env from './environment'
 
-const RINKEBY_HONEY_POT = '0x7777cd7c9c6d3537244871ac8e73b3cb9710d45a'
-const RINKEBY_STAGING_HONEY_POT = '0xeac000b64fc11a9ce6d885fe91fb4f9c2359cc21'
-const INSTANCE = env('INSTANCE')
 const ETH_NODE = env('ETH_NODE')
 
 const networks = {
@@ -20,7 +17,6 @@ const networks = {
     name: 'Rinkeby',
     type: 'rinkeby',
     defaultEthNode: ETH_NODE,
-    honeypot: getRinkebyHoneyPotAddress(INSTANCE),
     arbitrator: '0x35e7433141D5f7f2EB7081186f5284dCDD2ccacE',
     disputeManager: '0xc1f1c30878de30fd3ac3db7eacdd33a70c7110bd',
     subgraphs: {
@@ -38,7 +34,6 @@ const networks = {
     name: 'xDai',
     type: 'xdai',
     defaultEthNode: 'https://xdai.poanetwork.dev/',
-    honeypot: '0x8ccbeab14b5ac4a431fffc39f4bec4089020a155',
     arbitrator: '0x44E4fCFed14E1285c9e0F6eae77D5fDd0F196f85',
     disputeManager: '0xec7904e20b69f60966d6c6b9dc534355614dd922',
     subgraphs: {
@@ -65,13 +60,6 @@ export function getAvailableNetworks() {
     name,
     type,
   }))
-}
-
-function getRinkebyHoneyPotAddress(rinkebyInstance) {
-  if (rinkebyInstance === 'staging') {
-    return RINKEBY_STAGING_HONEY_POT
-  }
-  return RINKEBY_HONEY_POT
 }
 
 const agreementSubgraph = getNetwork().subgraphs?.agreement
