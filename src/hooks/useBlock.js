0093022229683ceab20c8390a2a7a22c07c4a4a6
fabriziovigevani <fabriziovigevani@gmail.com>
2021-05-27 12:54:06 -0300

    useBlock: Use Ref to save block number

--- src/hooks/useBlock.js
+++ src/hooks/useBlock.js
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from 'react'
+import { useEffect, useMemo, useRef, useState } from 'react'
 
 import { useMounted } from '@hooks/useMounted'
 import { useWallet } from '@providers/Wallet'
@@ -19,6 +19,7 @@ export function useLatestBlock() {
   const [block, setBlock] = useState({ number: 0, timestamp: 0 })
 
   const blockTime = useBlockTime()
+  const blockNumberRef = useRef(block.number)
 
   useEffect(() => {
     let timeoutId
@@ -26,7 +27,10 @@ export function useLatestBlock() {
     const pollBlock = async () => {
       try {
         const { number, timestamp } = await ethers.getBlock('latest')
-        setBlock({ number, timestamp })
+
+        if (number !== blockNumberRef.current) {
+          setBlock({ number, timestamp })
+        }
       } catch (err) {
         console.error('Error fetching block', err)
       }
