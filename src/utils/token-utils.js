2c55d634b0bb68802339dbb342129ba19f50087b
fabriziovigevani <fabriziovigevani@gmail.com>
2021-05-27 16:51:37 -0300

    Token utils: Clean unused code

--- src/utils/token-utils.js
+++ src/utils/token-utils.js
@@ -3,63 +3,12 @@ import defaultTokenSvg from '@assets/defaultTokenLogo.svg'
 import honeyIconSvg from '@assets/honey.svg'
 import stableTokenSvg from '@assets/stable-token.svg'
 
-const ANT_MAINNET_TOKEN_ADDRESS = '0x960b236A07cf122663c4303350609A66A7B288C0'
-const DAI_MAINNET_TOKEN_ADDRESS = '0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359'
-export const ETHER_TOKEN_FAKE_ADDRESS =
-  '0x0000000000000000000000000000000000000000'
-
-// "Important" tokens the Finance app should prioritize
-const PRESET_TOKENS = new Map([
-  [
-    'main',
-    [
-      ETHER_TOKEN_FAKE_ADDRESS,
-      ANT_MAINNET_TOKEN_ADDRESS,
-      DAI_MAINNET_TOKEN_ADDRESS,
-    ],
-  ],
-])
-
-// Some known tokens don’t strictly follow ERC-20 and it would be difficult to
-// adapt to every situation. The data listed in this map is used as a fallback
-// if either some part of their interface doesn't conform to a standard we
-// support.
-const KNOWN_TOKENS_FALLBACK = new Map([
-  [
-    'main',
-    new Map([
-      [
-        DAI_MAINNET_TOKEN_ADDRESS,
-        { symbol: 'DAI', name: 'Dai Stablecoin v1.0', decimals: '18' },
-      ],
-    ]),
-  ],
-])
-
 const LOCAL_TOKEN_ICONS = new Map([
   ['HNY', honeyIconSvg],
   ['HNYT', honeyIconSvg],
   ['DAI', stableTokenSvg],
   ['XDAI', stableTokenSvg],
 ])
-
-export const tokenDataFallback = (tokenAddress, fieldName, networkType) => {
-  // The fallback list is without checksums
-  const addressWithoutChecksum = tokenAddress.toLowerCase()
-
-  const fallbacksForNetwork = KNOWN_TOKENS_FALLBACK.get(networkType)
-  if (
-    fallbacksForNetwork == null ||
-    !fallbacksForNetwork.has(addressWithoutChecksum)
-  ) {
-    return null
-  }
-  return fallbacksForNetwork.get(addressWithoutChecksum)[fieldName] || null
-}
-
-export function getPresetTokens(networkType) {
-  return PRESET_TOKENS.get(networkType) || [ETHER_TOKEN_FAKE_ADDRESS]
-}
 
 export function formatDecimals(value, digits) {
   try {
@@ -97,7 +46,7 @@ export function formatTokenAmount(
   )
 }
 
-export function getTokenIconBySymbol(symbol) {
+export function getLocalTokenIconBySymbol(symbol) {
   return LOCAL_TOKEN_ICONS.get(symbol) || defaultTokenSvg
 }
 
