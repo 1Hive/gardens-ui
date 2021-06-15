a5a3b8953e71a257467b21de838563918c5f6310
fabriziovigevani <fabriziovigevani@gmail.com>
2021-06-15 17:16:20 -0300

    Remove route

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { Redirect, Route, Switch } from 'react-router-dom'
 import Garden from '@components/Garden'
 import Home from '@components/Home'
-import Onboarding from '@components/Onboarding'
 import Profile from '@components/Profile/Profile'
 
 export default function Routes() {
@@ -10,7 +9,6 @@ export default function Routes() {
     <Switch>
       <Redirect exact from="/" to="/home" />
       <Route path="/home" component={Home} />
-      <Route path="/create" component={Onboarding} />
       <Route exact path="/profile" component={Profile} />
       <Route path="/garden/:daoId" component={Garden} />
       <Redirect to="/home" />
