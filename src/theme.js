3b628f23e2b5f882758ab1461e0afa5000424ae6
Pavlo Mandryk <mandryk96@gmail.com>
2021-07-05 22:42:29 +0200

    Dark Mode: Add dark mode colors to theme

--- src/theme.js
+++ src/theme.js
@@ -2,6 +2,17 @@ export default {
   light: {
     _name: 'gardens',
     _appearance: 'light',
+
+    positiveContent: '#30DB9E',
+    positiveSurface: '#FFFFFF',
+
+    celeste: '#8253A8',
+    celesteBorder: '#8253A8',
+    celesteSurface: '#FCFAFF',
+
+    challenge: '#F5A623',
+    challengeBorder: '#F5A623',
+    challengeSurface: '#FFFDFA',
   },
   dark: {
     _name: 'gardens',
@@ -10,5 +21,41 @@ export default {
     background: '#031A3A',
     border: '#063374',
     surface: '#041F46',
+    contentSecondary: '#ACB5C2',
+
+    surfaceInteractive: '#05295D',
+    surfaceContent: '#DFE3EA',
+    surfaceIcon: '#DFE3EA',
+    surfaceUnder: '#51709E',
+
+    warning: '#FFA410',
+    warningSurface: '#363A3B',
+    warningSurfaceContent: '#FFA410',
+
+    info: '#00D2FF',
+    infoSurface: '#08436A',
+    infoSurfaceContent: '#18D4FB',
+
+    negative: '#FF6D6D',
+    negativeContent: '#FF6D6D',
+    negativeSurface: '#362F4E',
+    negativeSurfaceContent: '#FF6D6D',
+
+    accent: '#AAF5D4',
+    accentStart: '#7CE0D6',
+    accentEnd: '#AAF5D4',
+    accentContent: '#19364e',
+
+    positive: '#2BC58E',
+    positiveContent: '#2BC58E',
+    positiveSurface: '#0C4054',
+
+    celeste: '#B19AFF',
+    celesteBorder: '#2B235F',
+    celesteSurface: '#FCFAFF',
+
+    challenge: '#FFA410',
+    challengeBorder: '#FFA410',
+    challengeSurface: '#FFFDFA',
   },
 }
