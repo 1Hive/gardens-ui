18ada76d2e3daf802952f9797d3b5c8d049600ec
Pavlo Mandryk <mandryk96@gmail.com>
2021-07-05 22:36:39 +0200

    Dark Mode: Add Stepper images

--- src/components/Stepper/Step/Illustration.js
+++ src/components/Stepper/Step/Illustration.js
@@ -7,19 +7,35 @@ import {
   STEP_WORKING,
 } from '../stepper-statuses'
 // import TokenLoader from '../../TokenLoader'
+import { useAppTheme } from '@providers/AppTheme'
 
 import signRequestSuccessIllustration from '@assets/signRequestSuccess.svg'
 import signRequestFailIllustration from '@assets/signRequestFail.svg'
 import trxBeingMinedIllustration from '@assets/honey.svg'
+
+import signRequestSuccessIllustrationDark from '@assets/dark-mode/signRequestSuccess.svg'
+import signRequestFailIllustrationDark from '@assets/dark-mode/signRequestFail.svg'
+import trxBeingMinedIllustrationDark from '@assets/dark-mode/honey.svg'
 
 const illustrations = {
   [STEP_WORKING]: trxBeingMinedIllustration,
   [STEP_SUCCESS]: signRequestSuccessIllustration,
   [STEP_ERROR]: signRequestFailIllustration,
 }
+const illustrationsDarkMode = {
+  [STEP_WORKING]: trxBeingMinedIllustrationDark,
+  [STEP_SUCCESS]: signRequestSuccessIllustrationDark,
+  [STEP_ERROR]: signRequestFailIllustrationDark,
+}
 
 function Illustration({ status, index }) {
   const theme = useTheme()
+  const { appearance } = useAppTheme()
+
+  const illustrationsType =
+    appearance === 'light' ? illustrations : illustrationsDarkMode
+  console.log(appearance)
+
   return (
     <>
       {status === STEP_PROMPTING ? (
@@ -35,7 +51,7 @@ function Illustration({ status, index }) {
           `}
         />
       ) : (
-        <img src={illustrations[status]} height={96} width={96} />
+        <img src={illustrationsType[status]} height={96} width={96} />
       )}
     </>
   )
