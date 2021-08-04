ded62ac2e5cb79ecb364f8d4331c17f7be0db46f
Fabro <fabriziovigevani@gmail.com>
2021-08-03 20:17:33 -0500

    Use honey price oracle

--- src/components/Onboarding/Screens/HoneyswapLiquidity.js
+++ src/components/Onboarding/Screens/HoneyswapLiquidity.js
@@ -1,22 +1,55 @@
-import React from 'react'
-import { Box, GU, textStyle, useTheme } from '@1hive/1hive-ui'
-import { useOnboardingState } from '@providers/Onboarding'
+import React, { useCallback, useState } from 'react'
+import { Box, GU, TextInput, textStyle, useTheme } from '@1hive/1hive-ui'
 import Header from '../kit/Header'
 import Navigation from '../Navigation'
+import useHNYPriceOracle from '@hooks/useHNYPriceOracle'
+import { useOnboardingState } from '@providers/Onboarding'
+
+import { toDecimals } from '@utils/math-utils'
+
+const MIN_HNY_USD = 100
 
 function HoneyswapLiquidity() {
   const theme = useTheme()
+  const [hnyAmount, setHnyAmount] = useState('')
+  const [tokenAmount, setTokenAmount] = useState('')
   const { config, onBack, onNext, step, steps } = useOnboardingState()
   const tokenSymbol = config.tokens.symbol
+
+  const [hnyPrice] = useHNYPriceOracle(toDecimals('1', 18))
+
+  const handleHnyAmountChange = useCallback(event => {
+    const newAmount = event.target.value
+    if (isNaN(newAmount)) {
+      return
+    }
+
+    setHnyAmount(newAmount)
+  }, [])
+
+  const handleTokenAmountChange = useCallback(event => {
+    const newAmount = event.target.value
+    if (isNaN(newAmount)) {
+      return
+    }
+
+    setTokenAmount(newAmount)
+  }, [])
+
+  const liquidityProvided = hnyAmount && tokenAmount
+  const nextEnabled = liquidityProvided && hnyAmount * hnyPrice >= 100
 
   return (
     <div>
       <Header
-        title={`Configure HNY/${tokenSymbol} Pair`}
-        subtitle={`Set the initial HNY-${tokenSymbol} token equivalence to define the initial Honeyswap liquidity pair.`}
+        title="Configure HNY liquidity"
+        subtitle={`Set the initial HNY - ${tokenSymbol} token equivalence to define the initial Honeyswap liquidity pair.`}
       />
-
-      <div>
+      <div
+        css={`
+          margin-bottom: ${4 * GU}px;
+        `}
+      >
         <div>
           <Box padding={2 * GU}>
             <div>Input</div>
@@ -28,14 +61,19 @@ function HoneyswapLiquidity() {
                 margin-top: ${1.5 * GU}px;
               `}
             >
-              <span
+              <TextInput
+                placeholder="0.0"
+                value={hnyAmount}
+                onChange={handleHnyAmountChange}
+                wide
                 css={`
+                  border: 0;
+                  width: 100%;
+                  padding: 0;
                   ${textStyle('title3')};
                   color: ${theme.contentSecondary};
                 `}
-              >
-                0.3
-              </span>
+              />
               <span
                 css={`
                   ${textStyle('title4')};
@@ -51,28 +89,29 @@ function HoneyswapLiquidity() {
               color: ${theme.contentSecondary};
             `}
           >
-            <div
-              css={`
-                font-weight: bold;
-              `}
-            >
-              ≈ 100 USD
-              {/* TODO: Update 100 USD */}
-            </div>
+            {hnyAmount && (
+              <div
+                css={`
+                  font-weight: bold;
+                `}
+              >
+                ≈ {parseFloat(hnyPrice * hnyAmount, 2)} USD
+              </div>
+            )}
             <div
               css={`
                 ${textStyle('body3')};
               `}
             >
-              You must specify an amount worth at least 100 USD
+              You must specify an amount worth at least {MIN_HNY_USD} USD
             </div>
           </div>
         </div>
         <div
           css={`
             text-align: center;
-            ${textStyle('title3')};
-            margin: ${3 * GU}px 0;
+            ${textStyle('title2')};
+            margin: ${2 * GU}px 0;
           `}
         >
           <span>+</span>
@@ -88,14 +127,19 @@ function HoneyswapLiquidity() {
                 margin-top: ${1.5 * GU}px;
               `}
             >
-              <span
+              <TextInput
+                placeholder="0.0"
+                value={tokenAmount}
+                onChange={handleTokenAmountChange}
+                wide
                 css={`
+                  border: 0;
+                  width: 100%;
+                  padding: 0;
                   ${textStyle('title3')};
                   color: ${theme.contentSecondary};
                 `}
-              >
-                0.3
-              </span>
+              />
               <span
                 css={`
                   ${textStyle('title4')};
@@ -105,43 +149,47 @@ function HoneyswapLiquidity() {
               </span>
             </div>
           </Box>
-          <div
-            css={`
-              margin-top: ${3 * GU}px;
-              color: ${theme.contentSecondary};
-              ${textStyle('body3')};
-            `}
-          >
+          {liquidityProvided && (
             <div
               css={`
-                display: flex;
-                align-items: center;
-                justify-content: space-between;
+                margin-top: ${3 * GU}px;
+                color: ${theme.contentSecondary};
+                ${textStyle('body3')};
               `}
             >
-              <span>Initial price</span>
-              <span>1 {tokenSymbol} = 0.00257022 HNY</span>
-              {/* TODO: Upadate rates */}
+              <div
+                css={`
+                  display: flex;
+                  align-items: center;
+                  justify-content: space-between;
+                `}
+              >
+                <span>Initial price</span>
+                <span>
+                  1 {tokenSymbol} = {hnyAmount / tokenAmount} HNY
+                </span>
+              </div>
+              <div
+                css={`
+                  display: flex;
+                  align-items: center;
+                  justify-content: space-between;
+                  margin-top: ${1 * GU}px;
+                `}
+              >
+                <span>Liquidity provided</span>
+                <span>
+                  {tokenAmount} {tokenSymbol} + {hnyAmount} HNY
+                </span>
+              </div>
             </div>
-            <div
-              css={`
-                display: flex;
-                align-items: center;
-                justify-content: space-between;
-                margin-top: ${1 * GU}px;
-              `}
-            >
-              <span>Liquidity provided</span>
-              <span>100 {tokenSymbol} + 0.257022 HNY</span>
-              {/* TODO: Upadate liquidity provided */}
-            </div>
-          </div>
+          )}
         </div>
       </div>
 
       <Navigation
         backEnabled
-        nextEnabled
+        nextEnabled={nextEnabled}
         nextLabel={`Next: ${steps[step + 1].title}`}
         onBack={onBack}
         onNext={onNext}
