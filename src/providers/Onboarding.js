835da5d25d2c951c82a4c76b595db6157276fa2f
fabrizio <fabriziovigevani@gmail.com>
2021-08-24 12:31:35 -0500

    Move tx functions to own file and adjust values to required format

--- src/providers/Onboarding.js
+++ src/providers/Onboarding.js
@@ -6,15 +6,15 @@ import {
   calculateDecay,
   calculateWeight,
 } from '@utils/conviction-modelling-helpers'
-import { getNetwork } from '@/networks'
-import { getContract } from '@hooks/useContract'
 
-import { BYOT_TYPE, NATIVE_TYPE } from '@components/Onboarding/constants'
-import { encodeFunctionData } from '@utils/web3-utils'
-import { ZERO_ADDR } from '@/constants'
-
-import tokenAbi from '@abis/erc20.json'
-import templateAbi from '@abis/gardensTemplate.json'
+import { NATIVE_TYPE } from '@components/Onboarding/constants'
+import {
+  createGardenTxOne,
+  createGardenTxThree,
+  createGardenTxTwo,
+  createTokenApproveTxs,
+  createTokenHoldersTx,
+} from '@components/Onboarding/transaction-logic'
 
 const OnboardingContext = React.createContext()
 
@@ -144,132 +144,6 @@ OnboardingProvider.propTypes = {
 
 function useOnboardingState() {
   return useContext(OnboardingContext)
-}
-
-// TODO: See where to place them or keep them here
-function createTokenApproveTxs({ garden, liquidity, tokens }) {
-  const txs = []
-  const network = getNetwork()
-  const templateAddress = network.template
-  const honeyTokenAddress = network.honeyToken
-
-  txs.push(
-    createTokenTx(honeyTokenAddress, 'approve', [
-      templateAddress,
-      liquidity.honeyTokenLiquidity,
-    ])
-  )
-
-  if (garden.type === BYOT_TYPE) {
-    txs.push(
-      createTokenTx(tokens.address, 'approve', [
-        templateAddress,
-        liquidity.tokenLiquidity,
-      ])
-    )
-  }
-
-  return txs
-}
-
-function createGardenTxOne({ garden, issuance, liquidity, tokens, voting }) {
-  let existingToken, commonPool, gardenTokenLiquidity, existingTokenLiquidity
-
-  // New token
-  if (garden.type === NATIVE_TYPE) {
-    existingToken = ZERO_ADDR
-    gardenTokenLiquidity = liquidity.tokenLiquidity
-    existingTokenLiquidity = 0
-
-    // commonPool = totalSeedsAmount * initialRatio / (1 - initialRatio)
-    const totalSeedsAmount = tokens.holders.reduce(
-      (acc, [_, stake]) => acc + stake,
-      0
-    )
-    const initialRatio = issuance.initialRatio / 100
-    commonPool = (totalSeedsAmount * initialRatio) / (1 - initialRatio)
-  } else {
-    // Existing token
-    existingToken = tokens.address
-    gardenTokenLiquidity = 0
-    existingTokenLiquidity = liquidity.tokenLiquidity
-    commonPool = 0
-  }
-
-  return createTemplateTx('createGardenTxOne', [
-    existingToken,
-    tokens.name,
-    tokens.symbol,
-    [
-      commonPool,
-      liquidity.honeyTokenLiquidityStable,
-      gardenTokenLiquidity,
-      existingTokenLiquidity,
-    ],
-    [...voting],
-  ])
-}
-
-function createTokenHoldersTx({ tokens }) {
-  const accounts = tokens.holders.map(([account]) => account)
-  const stakes = tokens.holders.map(([_, stake]) => stake)
-
-  return createTemplateTx('createTokenHolders', [accounts, stakes])
-}
-
-function createGardenTxTwo({ conviction, issuance }) {
-  const requestToken = conviction.requestToken || ZERO_ADDR
-
-  return createTemplateTx('createGardenTxTwo', [
-    [issuance.targetRatio, issuance.maxAdjustmentRatioPerYear],
-    [
-      conviction.decay,
-      conviction.maxRatio,
-      conviction.weight,
-      conviction.minThresholdStakePct,
-    ],
-    requestToken,
-  ])
-}
-
-function createGardenTxThree({ agreement, garden }) {
-  const daoId = garden.name // TODO: Remember to do a check for the garden.name on the garden metadata screen, otherwise tx might fail if garden name already taken
-  const agreementContent = '' // TODO: Get the ipfs hash obtained from posting agreement.covenantFile to ipfs
-  const actionAmountStable = 0 // TODO: Use token liquidity to obtain stable value
-  const challengeAmountStable = 0 // TODO: Use token liquidity to obtain stable value
-
-  return createTemplateTx('createGardenTxThree', [
-    daoId,
-    agreement.title,
-    agreementContent,
-    agreement.challengePeriod,
-    [agreement.actionAmount, agreement.challengeAmount],
-    [actionAmountStable, actionAmountStable],
-    [challengeAmountStable, challengeAmountStable],
-  ])
-}
-
-function createTemplateTx(fn, params) {
-  const network = getNetwork()
-  const templateAddress = network.template
-  const templateContract = getContract(templateAddress, templateAbi)
-
-  const data = encodeFunctionData(templateContract, fn, params)
-
-  return {
-    to: templateAddress,
-    data,
-  }
-}
-
-function createTokenTx(tokenAddress, fn, params) {
-  const tokenContract = getContract(tokenAddress, tokenAbi)
-  const data = encodeFunctionData(tokenContract, fn, params)
-
-  return {
-    to: tokenAddress,
-    data,
-  }
 }
 
 export { OnboardingProvider, useOnboardingState }
