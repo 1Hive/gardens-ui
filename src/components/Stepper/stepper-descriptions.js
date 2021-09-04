b3a4acf9d0572c0acff059ebc0252e6f79da2547
yslcrypto <sacha.saint-leger@mail.mcgill.ca>
2021-09-04 17:05:46 +0200

    fix prettier

--- src/components/Stepper/stepper-descriptions.js
+++ src/components/Stepper/stepper-descriptions.js
@@ -9,8 +9,10 @@ import {
 export const TRANSACTION_SIGNING_DESC = {
   [STEP_WAITING]: 'Waiting for signature',
   [STEP_PROMPTING]: 'Waiting for signature',
-  [STEP_WORKING]: 'Hang tight. Your transaction is being processed by the network...',
-  [STEP_SUCCESS]: 'Your transaction has successfully been processed! You might need to wait a few seconds for the UI to update',
+  [STEP_WORKING]:
+    'Hang tight. Your transaction is being processed by the network...',
+  [STEP_SUCCESS]:
+    'Your transaction has successfully been processed! You might need to wait a few seconds for the UI to update',
   [STEP_ERROR]: 'An error has occured',
 }
 
