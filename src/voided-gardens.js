1cbece38dde9e6ee26fa9ba61d8ed156eccec649
Rodrigo Perez <rodrigopi1989@gmail.com>
2021-09-15 14:14:45 -0300

    void garden

---
+++ src/voided-gardens.js
@@ -0,0 +1,13 @@
+import { getNetworkType } from './utils/web3-utils'
+
+const XDAI_VOIDED = [['0x1baa8fd650c870fcfc773dd26e92b27b5b80d415', 'hello']]
+const RINKEBY_VOIDED = []
+
+const VOIDED_GARDENS = {
+  xdai: new Map(XDAI_VOIDED),
+  rinkeby: new Map(RINKEBY_VOIDED),
+}
+
+export function getVoidedGardensByNetwork() {
+  return VOIDED_GARDENS[getNetworkType()]
+}
