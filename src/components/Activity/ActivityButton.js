a553cae4ffdc09d3bf0f2ac0cd452f385c5044cc
Rodrigo Perez <rodrigopi1989@gmail.com>
2021-09-18 04:28:31 -0300

    remove console.log

--- src/components/Activity/ActivityButton.js
+++ src/components/Activity/ActivityButton.js
@@ -16,7 +16,7 @@ const ActivityButton = React.memo(function ActivityButton() {
   const theme = useTheme()
   const [opened, setOpened] = useState(false)
   const { activities, markActivitiesRead, unreadCount } = useActivity()
-  console.log('activity button ', unreadCount)
+
   const containerRef = useRef()
   const popoverFocusElement = useRef()
 
