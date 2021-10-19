399f06fabaa40911ea2c1303a417a52fd155d201
PJColombo <paulo.colombo@pm.me>
2021-10-20 01:23:00 +0200

    Add signed gardens to sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,28 +1,44 @@
 import React, { useMemo } from 'react'
-import { GU, useTheme, Link } from '@1hive/1hive-ui'
+import { useTrail, animated } from 'react-spring'
+import { GU, Link, useTheme } from '@1hive/1hive-ui'
 import gardensLogo from '@assets/gardensLogoMark.svg'
 import defaultGardenLogo from '@assets/defaultGardenLogo.png'
-import MenuItem from './MenuItem'
+import useUser from '@/hooks/useUser'
 import { useGardens } from '@/providers/Gardens'
+import { useWallet } from '@/providers/Wallet'
+import { addressesEqual } from '@/utils/web3-utils'
+import LoadingRing from '../LoadingRing'
+import MenuItem from './MenuItem'
 
 function Sidebar() {
   const theme = useTheme()
+  const { account } = useWallet()
+  const [connectedUser, userLoading] = useUser(account)
   const { connectedGarden, gardens } = useGardens()
+  const sidebarGardens = useMemo(() => {
+    if (!gardens || !connectedUser?.gardensSigned) {
+      return []
+    }
 
-  const gardenData = useMemo(
-    () =>
-      gardens
-        .map(garden => {
-          return {
-            name: garden.name,
-            address: garden.address,
-            path: `#/garden/${garden.address}`,
-            src: garden.logo || defaultGardenLogo,
-          }
-        })
-        .filter(garden => garden.address === connectedGarden.address),
-    [connectedGarden, gardens]
-  )
+    return connectedUser.gardensSigned.map(gardenSignedAddress => {
+      const { address, name, logo } = gardens.find(g =>
+        addressesEqual(g.address, gardenSignedAddress)
+      )
+      return {
+        address,
+        name,
+        path: `#/garden/${address}`,
+        src: logo || defaultGardenLogo,
+      }
+    })
+  }, [gardens, connectedUser])
+  const startTrail = sidebarGardens.length > 0
+  const trail = useTrail(sidebarGardens.length, {
+    config: { mass: 5, tension: 2300, friction: 150 },
+    opacity: startTrail ? 1 : 0,
+    left: startTrail ? '0' : '-40px',
+    from: { left: '-40px', opacity: 0 },
+  })
 
   return (
     <div
@@ -65,18 +81,49 @@ function Sidebar() {
           </Link>
         </div>
       </div>
-      <nav>
-        <ul>
-          {gardenData.map((garden, i) => (
-            <MenuItem
-              key={garden.address}
-              active={garden.address === connectedGarden.address}
-              path={garden.path}
-              name={garden.name}
-              src={garden.src}
-            />
-          ))}
-        </ul>
+      <nav
+        css={`
+          height: 100vh;
+          overflow-y: scroll;
+          -ms-overflow-style: none; /* IE and Edge */
+          scrollbar-width: none; /* Firefox */
+          &::-webkit-scrollbar {
+            display: none;
+          }
+        `}
+      >
+        {userLoading ? (
+          <div
+            css={`
+              display: flex;
+              justify-content: center;
+              margin-top: ${6 * GU}px;
+            `}
+          >
+            <LoadingRing mode="half-circle" />
+          </div>
+        ) : (
+          <div>
+            <ul>
+              {trail.map((style, index) => {
+                const { address, name, path, src } = sidebarGardens[index]
+                return (
+                  <animated.div
+                    key={sidebarGardens[index].address}
+                    style={{ position: 'relative', ...style }}
+                  >
+                    <MenuItem
+                      active={addressesEqual(address, connectedGarden?.address)}
+                      name={name}
+                      path={path}
+                      src={src}
+                    />
+                  </animated.div>
+                )
+              })}
+            </ul>
+          </div>
+        )}
       </nav>
     </div>
   )
