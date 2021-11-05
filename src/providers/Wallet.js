ceb3910d3444dec629536216086beaaaf65ba3e4
0xGabi <gabrielpk.18@gmail.com>
2021-11-05 11:19:53 -0300

    Feat(wallet): refactor logic to new use-wallet API

--- src/providers/Wallet.js
+++ src/providers/Wallet.js
@@ -1,52 +1,129 @@
-import React, { useContext, useMemo } from 'react'
+import React, { useCallback, useContext, useMemo, useState } from 'react'
+import PropTypes from 'prop-types'
 import { providers as EthersProviders } from 'ethers'
-import { UseWalletProvider, useWallet } from 'use-wallet'
-import { getUseWalletConnectors, getDefaultProvider } from '@utils/web3-utils'
-import { getNetwork } from '@/networks'
+import {
+  chains,
+  ChainUnsupportedError,
+  UseWalletProvider,
+  useWallet as useWalletBase,
+} from 'use-wallet'
+
+import { getDefaultProvider } from '@utils/web3-utils'
+import { getEthersNetwork } from '@/networks'
 import { getDefaultChain } from '@/local-settings'
+import { useWalletConnectors } from '@/ethereum-providers/connectors'
+import { LocalStorageWrapper } from '@/local-storage-wrapper'
+
+export const WALLET_STATUS = Object.freeze({
+  providers: 'providers',
+  network: 'network',
+  connecting: 'connecting',
+  connected: 'connected',
+  disconnected: 'disconnected',
+  error: 'error',
+})
+
+const NETWORK_TYPE_DEFAULT = chains.getChainInformation(getDefaultChain())?.type
 
 const WalletAugmentedContext = React.createContext()
 
-function useWalletAugmented() {
-  return useContext(WalletAugmentedContext)
-}
-
 // Adds Ethers.js to the useWallet() object
 function WalletAugmented({ children }) {
-  const wallet = useWallet()
-  const { ethereum } = wallet
+  const {
+    account,
+    balance,
+    ethereum,
+    connector,
+    status,
+    chainId,
+    providerInfo,
+    type,
+    networkName,
+    ...walletBaseRest
+  } = useWalletBase()
+
+  const initialNetwork = useMemo(() => {
+    const lastNetwork = LocalStorageWrapper.get('last-network', false)
+    if (!lastNetwork) return NETWORK_TYPE_DEFAULT
+    return lastNetwork
+  }, [])
+
+  const [disconnectedNetworkType, setDisconnectedNetworkType] = useState(
+    initialNetwork
+  )
+
+  const connected = useMemo(() => status === 'connected', [status])
+  const networkType = useMemo(() => {
+    const newNetwork = connected ? networkName : disconnectedNetworkType
+    LocalStorageWrapper.set('last-network', newNetwork, false)
+    return newNetwork
+  }, [connected, networkName, disconnectedNetworkType])
+
+  const changeNetworkTypeDisconnected = useCallback(
+    newNetworkType => {
+      if (status === 'disconnected') {
+        setDisconnectedNetworkType(newNetworkType)
+      }
+    },
+    [status]
+  )
 
   const ethers = useMemo(() => {
     if (!ethereum) {
       return getDefaultProvider()
     }
 
-    const ensRegistry = getNetwork()?.ensRegistry
-    return new EthersProviders.Web3Provider(ethereum, {
-      name: '',
-      chainId: getDefaultChain(),
-      ensAddress: ensRegistry,
-    })
+    return new EthersProviders.Web3Provider(ethereum, getEthersNetwork())
   }, [ethereum])
 
-  const contextValue = useMemo(() => ({ ...wallet, ethers }), [wallet, ethers])
+  const wallet = useMemo(
+    () => ({
+      account,
+      balance,
+      ethereum,
+      ethers,
+      networkType,
+      providerInfo: providerInfo,
+      status,
+      chainId: connected ? chainId : getDefaultChain(), // connect to default chain if wallet is not connected
+      connected,
+      changeNetworkTypeDisconnected,
+      ...walletBaseRest,
+    }),
+    [
+      account,
+      balance,
+      ethereum,
+      ethers,
+      networkType,
+      providerInfo,
+      status,
+      chainId,
+      walletBaseRest,
+      connected,
+      changeNetworkTypeDisconnected,
+    ]
+  )
 
   return (
-    <WalletAugmentedContext.Provider value={contextValue}>
+    <WalletAugmentedContext.Provider value={wallet}>
       {children}
     </WalletAugmentedContext.Provider>
   )
 }
+WalletAugmentedContext.propTypes = { children: PropTypes.node }
 
-function WalletProvider({ children }) {
-  const chainId = getDefaultChain()
-
-  const connectors = getUseWalletConnectors()
+export function WalletProvider({ children }) {
   return (
-    <UseWalletProvider chainId={chainId} connectors={connectors}>
+    <UseWalletProvider connectors={useWalletConnectors} autoConnect>
       <WalletAugmented>{children}</WalletAugmented>
     </UseWalletProvider>
   )
 }
+WalletProvider.propTypes = { children: PropTypes.node }
 
-export { useWalletAugmented as useWallet, WalletProvider }
+export function useWallet() {
+  return useContext(WalletAugmentedContext)
+}
+
+export { ChainUnsupportedError }
