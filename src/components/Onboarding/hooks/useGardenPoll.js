44ade2e979fea50dfa00735bc9c660040ba5b55e
fabriziovigevani <fabriziovigevani@gmail.com>
2021-11-29 23:49:22 -0300

    Onboarding: Poll newly created garden to signal deployment completion

---
+++ src/components/Onboarding/hooks/useGardenPoll.js
@@ -0,0 +1,38 @@
+import { useEffect } from 'react'
+import { useWallet } from '@providers/Wallet'
+import { getGarden } from '@1hive/connect-gardens'
+import { getNetwork } from '@/networks'
+
+const noop = () => {}
+
+// Onboarding local hook to poll for the new created garden
+export default function useGardenPoll(gardenAddress, onResult = noop) {
+  const { preferredNetwork } = useWallet()
+  const { subgraphs } = getNetwork(preferredNetwork)
+
+  // Poll garden until we can confirm it was picked up by the subgraph.
+  useEffect(() => {
+    let timer
+
+    const pollGarden = async () => {
+      try {
+        // Note that getGarden throws an error if it can't find the garden for the given address
+        await getGarden(
+          { network: preferredNetwork, subgraphUrl: subgraphs.gardens },
+          gardenAddress
+        )
+
+        onResult()
+      } catch (err) {
+        console.error(err)
+        timer = setTimeout(pollGarden, 2500)
+      }
+    }
+
+    if (gardenAddress) {
+      pollGarden(gardenAddress)
+    }
+
+    return () => clearTimeout(timer)
+  }, [gardenAddress, onResult, preferredNetwork, subgraphs])
+}
