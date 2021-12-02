b1107b3f3776fed41ba90345868db3517a2ab341
fabriziovigevani <fabriziovigevani@gmail.com>
2021-12-02 19:42:53 -0300

    App: Remove duplicate Activity provider

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import MainView from '@components/MainView'
 import Routes from './routes/Routes'
 import WelcomeLoader from '@components/Welcome/WelcomeLoader'
 
-import { ActivityProvider } from './providers/ActivityProvider'
 import { GardensProvider } from './providers/Gardens'
 import { ProfileProvider } from './providers/Profile'
 import { UserProvider } from './providers/User'
@@ -19,18 +18,16 @@ function App() {
       <Main assetsUrl="/aragon-ui/" layout={false} scrollView={false}>
         <WalletProvider>
           <GlobalErrorHandler>
-            <ActivityProvider>
-              <ProfileProvider>
-                <UserProvider>
-                  <GardensProvider>
-                    <WelcomeLoader />
-                    <MainView>
-                      <Routes />
-                    </MainView>
-                  </GardensProvider>
-                </UserProvider>
-              </ProfileProvider>
-            </ActivityProvider>
+            <ProfileProvider>
+              <UserProvider>
+                <GardensProvider>
+                  <WelcomeLoader />
+                  <MainView>
+                    <Routes />
+                  </MainView>
+                </GardensProvider>
+              </UserProvider>
+            </ProfileProvider>
           </GlobalErrorHandler>
         </WalletProvider>
       </Main>
