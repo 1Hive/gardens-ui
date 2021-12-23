29e4be04ff0c16330e8bd5a602c8dba2d35fee0f
Tiago Moutinho <tiago.moutinho@olx.com>
2021-12-23 00:25:09 +0000

    Fix deploy failure

--- src/voided-gardens.ts
+++ src/voided-gardens.ts
@@ -1,17 +1,11 @@
 import { getNetworkType } from './utils/web3-utils'
 
-const POLYGON_VOIDED: [string, string][] = []
-const XDAI_VOIDED: [string, string][] = [
-  ['0x9ceac080e12ab6700eef58c45fa02a50e8e2cd4b', '_'],
-]
-const RINKEBY_VOIDED: [string, string][] = []
-
 const VOIDED_GARDENS: {
   [x: string]: Map<string, string>
 } = {
-  polygon: new Map(POLYGON_VOIDED),
-  xdai: new Map(XDAI_VOIDED),
-  rinkeby: new Map(RINKEBY_VOIDED),
+  polygon: new Map([]),
+  xdai: new Map([['0x9ceac080e12ab6700eef58c45fa02a50e8e2cd4b', '_']]),
+  rinkeby: new Map([]),
 }
 
 export function getVoidedGardensByNetwork() {
