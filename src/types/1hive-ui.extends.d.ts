5ffaa49d9de341d11213c0875b3103336f11ae28
Felipe Novaes Rocha <windholyghost@gmail.com>
2021-12-24 03:46:47 -0300

    fix: adding other while npm not published

--- src/types/1hive-ui.extends.d.ts
+++ src/types/1hive-ui.extends.d.ts
@@ -14,4 +14,41 @@ declare module '@1hive/1hive-ui' {
     children?: ReactNode
     padding?: number
   }): JSX.Element
+
+  //
+  //
+  // Remove those below after publish new version d.ts types 1hive-ui with the same
+  //
+  //
+  export declare function Modal({
+    children,
+    onClose,
+    onClosed,
+    padding,
+    visible,
+    width,
+    closeButton,
+    ...props
+  }: {
+    [x: string]: any
+    children?: any
+    onClose?: () => void
+    onClosed?: () => void
+    padding?: any
+    visible?: any
+    width?: any
+    closeButton?: any
+  }): JSX.Element
+
+  export declare function Link({
+    onClick,
+    href,
+    external,
+    ...props
+  }: {
+    [x: string]: any
+    onClick?: () => void
+    href?: any
+    external?: any
+  }): JSX.Element
 }
