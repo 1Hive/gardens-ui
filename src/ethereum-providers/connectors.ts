086ab83d1d777b92ec65ecb398c65b92c5c5a80d
Tiago Moutinho <tiago.moutinho@olx.com>
2021-12-26 20:38:12 +0000

    rollback of some code

--- src/ethereum-providers/connectors.ts
+++ src/ethereum-providers/connectors.ts
@@ -46,6 +46,10 @@ export const CONNECTORS = [
 // the final data that we pass to use-wallet package.
 export const useWalletConnectors = CONNECTORS.reduce((acc, connector) => {
   return (
-    connector !== null && { ...acc, [connector.id]: connector.properties ?? {} }
+    (connector !== null && {
+      ...acc,
+      [connector.id]: connector.properties ?? {},
+    }) ??
+    acc
   )
 }, {})
