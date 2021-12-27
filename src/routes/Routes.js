c56d09a5ff8fb73cff209cb84eb4caf7599e4777
Anup <allamsetty.anup@gmail.com>
2021-12-27 11:39:12 +0000

    Add support for route-based code splitting by using react lazy imports

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,17 +1,32 @@
-import React from 'react'
+import React, { Suspense, lazy } from 'react'
 import { Redirect, Route, Switch } from 'react-router-dom'
-import Garden from '@components/Garden'
-import Home from '@components/Home'
-import Profile from '@components/Profile/Profile'
+
+import { LoadingRing, Main } from '@1hive/1hive-ui'
+
+// import Garden from '@components/Garden'
+// import Home from '@components/Home'
+// import Profile from '@components/Profile/Profile'
+
+const Home = lazy(() => import('@components/Home'))
+const Garden = lazy(() => import('@components/Garden'))
+const Profile = lazy(() => import('@components/Profile/Profile'))
 
 export default function Routes() {
   return (
     <Switch>
-      <Redirect exact from="/" to="/home" />
-      <Route path="/home" component={Home} />
-      <Route exact path="/profile" component={Profile} />
-      <Route path="/:networkType/garden/:gardenAddress" component={Garden} />
-      <Redirect to="/home" />
+      <Suspense
+        fallback={
+          <Main>
+            <LoadingRing />
+          </Main>
+        }
+      >
+        <Redirect exact from="/" to="/home" />
+        <Route path="/home" component={Home} />
+        <Route exact path="/profile" component={Profile} />
+        <Route path="/:networkType/garden/:gardenAddress" component={Garden} />
+        <Redirect to="/home" />
+      </Suspense>
     </Switch>
   )
 }
