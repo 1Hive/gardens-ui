2f457533594627aff6b43ca45b8b75628564abbe
Anup <allamsetty.anup@gmail.com>
2021-12-27 14:06:24 +0000

    Codesplit garden routes

--- src/routes/garden/Routes.js
+++ src/routes/garden/Routes.js
@@ -1,19 +1,32 @@
-import React from 'react'
+import React, { Suspense, lazy } from 'react'
 import { Route, Switch } from 'react-router-dom'
-import Agreement from '@components/Garden/Agreement/Agreement'
-import DecisionLoader from '@components/Garden/DecisionLoader'
-import Home from '@components/Garden/Home'
-import ProposalLoader from '@components/Garden/ProposalLoader'
-import StakeManagement from '@components/Garden/Stake/StakeManagement'
+
+import { LoadingRing, Main } from '@1hive/1hive-ui'
+
+const Home = lazy(() => import('@components/Garden/Home'))
+const ProposalLoader = lazy(() => import('@components/Garden/ProposalLoader'))
+const DecisionLoader = lazy(() => import('@components/Garden/DecisionLoader'))
+const Agreement = lazy(() => import('@components/Garden/Agreement/Agreement'))
+const StakeManagement = lazy(() =>
+  import('@components/Garden/Stake/StakeManagement')
+)
 
 export default function Routes() {
   return (
     <Switch>
-      <Route exact path="*/proposal/:id" component={ProposalLoader} />
-      <Route exact path="*/vote/:id" component={DecisionLoader} />
-      <Route exact path="*/covenant" component={Agreement} />
-      <Route exact path="*/collateral*" component={StakeManagement} />
-      <Route path="*/" component={Home} />
+      <Suspense
+        fallback={
+          <Main>
+            <LoadingRing />
+          </Main>
+        }
+      >
+        <Route exact path="*/proposal/:id" component={ProposalLoader} />
+        <Route exact path="*/vote/:id" component={DecisionLoader} />
+        <Route exact path="*/covenant" component={Agreement} />
+        <Route exact path="*/collateral*" component={StakeManagement} />
+        <Route path="*/" component={Home} />
+      </Suspense>
     </Switch>
   )
 }
