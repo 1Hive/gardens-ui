cfe9af3e95893bd6a98942aadbf2d82c59b988f7
Anup <allamsetty.anup@gmail.com>
2021-12-27 17:38:58 +0000

    Test react-scripts v5

--- config-overrides.js
+++ config-overrides.js
@@ -1,7 +1,14 @@
 /* config-overrides.js */
 
 const path = require('path')
-const { addWebpackAlias, useBabelRc, override } = require('customize-cra')
+const {
+  addWebpackAlias,
+  addWebpackPlugin,
+  addWebpackResolve,
+  useBabelRc,
+  override,
+} = require('customize-cra')
+const webpack = require('webpack')
 
 module.exports = override(
   useBabelRc(),
@@ -14,5 +21,28 @@ module.exports = override(
     '@lib': path.resolve(__dirname, './src/lib'),
     '@providers': path.resolve(__dirname, './src/providers'),
     '@utils': path.resolve(__dirname, './src/utils'),
+  }),
+  addWebpackPlugin(
+    new webpack.ProvidePlugin({
+      Buffer: ['buffer', 'Buffer'],
+    })
+  ),
+  addWebpackPlugin(
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+    })
+  ),
+  addWebpackResolve({
+    fallback: {
+      url: require.resolve('url/'),
+      assert: require.resolve('assert/'),
+      buffer: require.resolve('buffer/'),
+      http: require.resolve('stream-http'),
+      path: require.resolve('path-browserify'),
+      https: require.resolve('https-browserify'),
+      os: require.resolve('os-browserify/browser'),
+      stream: require.resolve('stream-browserify'),
+      timers: require.resolve('timers-browserify'),
+    },
   })
 )
