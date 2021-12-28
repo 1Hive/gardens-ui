562cc1149c2a94808cde7f5655423dd1411128f9
Anup <allamsetty.anup@gmail.com>
2021-12-28 22:22:18 +0000

    Remove internal suspense layer

--- src/routes/garden/Routes.js
+++ src/routes/garden/Routes.js
@@ -1,7 +1,5 @@
-import React, { Suspense, lazy } from 'react'
+import React, { lazy } from 'react'
 import { Route, Switch } from 'react-router-dom'
-
-import { LoadingRing, Main } from '@1hive/1hive-ui'
 
 const Home = lazy(() => import('@components/Garden/Home'))
 const ProposalLoader = lazy(() => import('@components/Garden/ProposalLoader'))
@@ -14,19 +12,11 @@ const StakeManagement = lazy(() =>
 export default function Routes() {
   return (
     <Switch>
-      <Suspense
-        fallback={
-          <Main>
-            <LoadingRing />
-          </Main>
-        }
-      >
-        <Route exact path="*/proposal/:id" component={ProposalLoader} />
-        <Route exact path="*/vote/:id" component={DecisionLoader} />
-        <Route exact path="*/covenant" component={Agreement} />
-        <Route exact path="*/collateral*" component={StakeManagement} />
-        <Route path="*/" component={Home} />
-      </Suspense>
+      <Route exact path="*/proposal/:id" component={ProposalLoader} />
+      <Route exact path="*/vote/:id" component={DecisionLoader} />
+      <Route exact path="*/covenant" component={Agreement} />
+      <Route exact path="*/collateral*" component={StakeManagement} />
+      <Route path="*/" component={Home} />
     </Switch>
   )
 }
