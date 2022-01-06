9001b96df1617e5d5c9eb8f316d32b802cc01166
Felipe Novaes Rocha <windholyghost@gmail.com>
2022-01-06 00:13:35 -0300

    fix: (wip) change methods to point to services, typescript support.

--- src/services/github.ts
+++ src/services/github.ts
@@ -1,33 +1,43 @@
 import { getNetworkName } from '@utils/web3-utils'
 import env from '@/environment'
 
-const ASSETS_FOLDER_BASE =
-  'https://raw.githubusercontent.com/1Hive/dao-list/master/assets'
-const ENDPOINT_BASE = 'https://api.github.com/repos/1Hive/dao-list'
-const GITHUB_API_TOKEN = env('GITHUB_API_TOKEN')
+const OWNER_REPO = env('OWNER_REPO_DAO_LIST') === null ? 'kamikazebr' : '1Hive' //TODO: Change or remove it after tests
+
+const ASSETS_FOLDER_BASE = `https://raw.githubusercontent.com/${OWNER_REPO}/dao-list/master/assets`
+const ENDPOINT_BASE = `https://api.github.com/repos/${OWNER_REPO}/dao-list`
+const ANOTHER_ENDPOINT_BASE = 'http://localhost:3001' // TODO: Migrate to ENV vars and change the name the var.
 
 // This step must be called after the dao is published and we have the dao address
-export async function publishNewDao(daoAddress, daoMetadata, chainId) {
+export async function publishNewDao(
+  daoAddress: string,
+  daoMetadata: GardenMetadataType,
+  chainId: number
+) {
   try {
-    const { data: fileContent } = await fetchFileContent(chainId)
+    const { data: fileContent, error: errorFileContent } =
+      await fetchFileContent(chainId)
+    if (errorFileContent || !fileContent) {
+      throw new Error('fetchFileContent some error') //TODO It should threat errors different, without return error bolean
+    }
     await publishDaoAssets(daoMetadata)
 
     const newDaoList = fileContent.gardens
+
     newDaoList.push({
       address: daoAddress,
       name: daoMetadata.name,
       description: daoMetadata.description,
       forum: daoMetadata.forum,
       links: daoMetadata.links,
-      logo:
-        daoMetadata.logo &&
-        `${ASSETS_FOLDER_BASE}/${daoMetadata.name}/logo.${daoMetadata.logo.imageExtension}`,
-      logo_type:
-        daoMetadata.logo_type &&
-        `${ASSETS_FOLDER_BASE}/${daoMetadata.name}/logo_type.${daoMetadata.logo_type.imageExtension}`,
-      token_logo:
-        daoMetadata.token_logo &&
-        `${ASSETS_FOLDER_BASE}/${daoMetadata.name}/token_logo.${daoMetadata.token_logo.imageExtension}`,
+      logo: daoMetadata.logo
+        ? `${ASSETS_FOLDER_BASE}/${daoMetadata.name}/logo.${daoMetadata.logo.imageExtension}`
+        : null,
+      logo_type: daoMetadata.logo_type
+        ? `${ASSETS_FOLDER_BASE}/${daoMetadata.name}/logo_type.${daoMetadata.logo_type.imageExtension}`
+        : null,
+      token_logo: daoMetadata.token_logo
+        ? `${ASSETS_FOLDER_BASE}/${daoMetadata.name}/token_logo.${daoMetadata.token_logo.imageExtension}`
+        : null,
     })
     const newContent = {
       ...fileContent,
@@ -37,19 +47,15 @@ export async function publishNewDao(daoAddress, daoMetadata, chainId) {
     const { data: latestCommitSha } = await fetchLatestCommitSha()
     const { data: baseTreSha } = await fetchBaseTreeSha(latestCommitSha)
 
-    const { data: newTreeSha } = await createTree(
+    const networkName = getNetworkName(chainId).toLowerCase()
+
+    await createTreeCommitAndChangeHeads({
       baseTreSha,
-      newContent,
-      chainId
-    )
-
-    const { data: commitSha } = await createCommit(
       latestCommitSha,
-      newTreeSha,
-      daoMetadata.name
-    )
-
-    await changeHeadsCommitSha(commitSha)
+      daoMetadataName: daoMetadata.name,
+      newContent,
+      networkName,
+    })
   } catch (error) {
     console.error(error)
   }
@@ -61,7 +67,6 @@ const fetchLatestCommitSha = async () => {
     const result = await fetch(endpoint, {
       method: 'GET',
       headers: {
-        Authorization: `token ${GITHUB_API_TOKEN}`,
         Accept: 'application/vnd.github.v3+json',
         'Content-Type': 'application/json',
       },
@@ -75,13 +80,12 @@ const fetchLatestCommitSha = async () => {
   }
 }
 
-const fetchBaseTreeSha = async commitSha => {
+const fetchBaseTreeSha = async (commitSha: string) => {
   const endpoint = `${ENDPOINT_BASE}/git/commits/${commitSha}`
   try {
     const result = await fetch(endpoint, {
       method: 'GET',
       headers: {
-        Authorization: `token ${GITHUB_API_TOKEN}`,
         Accept: 'application/vnd.github.v3+json',
         'Content-Type': 'application/json',
       },
@@ -95,79 +99,67 @@ const fetchBaseTreeSha = async commitSha => {
   }
 }
 
-export const createTree = async (baseTreSha, fileContent, chainId) => {
-  const endpoint = `${ENDPOINT_BASE}/git/trees`
-  const network = getNetworkName(chainId).toLowerCase()
+type TreeCommitParams = {
+  baseTreSha: string
+  latestCommitSha: string
+  daoMetadataName: string
+  newContent: any
+  networkName: string
+}
+
+export const createTreeCommitAndChangeHeads = async ({
+  baseTreSha,
+  latestCommitSha,
+  daoMetadataName,
+  newContent,
+  networkName,
+}: TreeCommitParams) => {
+  const endpoint = `${ANOTHER_ENDPOINT_BASE}/v1/dao`
 
   const bodyData = {
-    base_tree: baseTreSha,
-    tree: [
-      {
-        path: `${network}.json`,
-        mode: '100644',
-        type: 'blob',
-        content: JSON.stringify(fileContent, null, 4),
-      },
-    ],
+    baseTreSha,
+    latestCommitSha,
+    daoMetadataName,
+    newContent,
+    networkName,
   }
+
   try {
     const result = await fetch(endpoint, {
       method: 'POST',
       headers: {
-        Authorization: `token ${GITHUB_API_TOKEN}`,
         'Content-Type': 'application/json',
       },
       body: JSON.stringify(bodyData),
     })
     const data = await result.json()
-
-    return { data: data.sha, error: !result.ok }
+    console.log(data)
+    return { data: data, error: !result.ok }
   } catch (err) {
-    console.error(`Error requesting tree sha`, err)
+    console.error(`Error requesting createTreeCommitAndChangeHeads`, err)
     return { error: true }
   }
 }
 
-const createCommit = async (latestCommitSha, newTreeSha, daoName) => {
-  const endpoint = `${ENDPOINT_BASE}/git/commits`
-  const bodyData = {
-    parents: [latestCommitSha],
-    tree: newTreeSha,
-    message: ` ${daoName} added`,
-  }
-  try {
-    const result = await fetch(endpoint, {
-      method: 'POST',
-      headers: {
-        Authorization: `token ${GITHUB_API_TOKEN}`,
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(bodyData),
-    })
-    const data = await result.json()
-
-    return { data: data.sha, error: !result.ok }
-  } catch (err) {
-    console.error(`Error requesting tree sha`, err)
-    return { error: true }
-  }
-}
-
-const createFileContent = async (folderName, fileName, base64, commitMsg) => {
-  const endpoint = `${ENDPOINT_BASE}/contents/assets/${folderName}/${fileName}`
+const createFileContent = async (
+  folderName: string,
+  fileName: string,
+  base64: string,
+  commitMsg: string
+) => {
+  const endpoint = `${ANOTHER_ENDPOINT_BASE}/v1/daoAssets`
 
   const bodyData = {
-    owner: '1Hive',
-    repo: 'dao-list',
     path: fileName,
     message: commitMsg,
     content: base64,
+    folderName,
   }
+
   try {
     const result = await fetch(endpoint, {
       method: 'PUT',
       headers: {
-        Authorization: `token ${GITHUB_API_TOKEN}`,
         'Content-Type': 'application/json',
       },
       body: JSON.stringify(bodyData),
@@ -181,30 +173,46 @@ const createFileContent = async (folderName, fileName, base64, commitMsg) => {
   }
 }
 
-const changeHeadsCommitSha = async commitSha => {
-  const endpoint = `${ENDPOINT_BASE}/git/refs/heads/master`
-  const bodyData = {
-    sha: commitSha,
-  }
-  try {
-    const result = await fetch(endpoint, {
-      method: 'POST',
-      headers: {
-        Authorization: `token ${GITHUB_API_TOKEN}`,
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(bodyData),
-    })
-    const data = await result.json()
-
-    return { data: data, error: !result.ok }
-  } catch (err) {
-    console.error(`Error requesting tree sha`, err)
-    return { error: true }
-  }
+type Base64ExtType = {
+  imageExtension: string
+  base64: string
 }
 
-export const fetchFileContent = async chainId => {
+type LinksMetadadaType = {
+  community?: Array<Record<string, unknown>>
+  documentation?: Array<Record<string, unknown>>
+}
+
+type GardenMetadataType = {
+  address: string
+  name: string
+  description: string
+  forum: string
+  links?: LinksMetadadaType
+  logo?: Base64ExtType
+  logo_type?: Base64ExtType
+  token_logo?: Base64ExtType
+}
+
+type GardenMetadataGithubType = Omit<
+  GardenMetadataType,
+  'logo' | 'logo_type' | 'token_logo'
+> & {
+  logo: string | null
+  logo_type: string | null
+  token_logo: string | null
+}
+
+type FileContentMetadataGithub = {
+  data?: {
+    gardens: Array<GardenMetadataGithubType>
+  }
+  error: boolean
+}
+
+export const fetchFileContent = async (
+  chainId: number
+): Promise<FileContentMetadataGithub> => {
   const network = getNetworkName(chainId).toLowerCase()
   const endpoint = `${ENDPOINT_BASE}/contents/${network}.json`
 
@@ -212,7 +220,6 @@ export const fetchFileContent = async chainId => {
     const result = await fetch(endpoint, {
       method: 'GET',
       headers: {
-        Authorization: `token ${GITHUB_API_TOKEN}`,
         Accept: 'application/vnd.github.VERSION.raw',
         'Content-Type': 'application/json',
       },
@@ -232,7 +239,7 @@ export const fetchFileContent = async chainId => {
   }
 }
 
-const publishDaoAssets = async daoMetadata => {
+const publishDaoAssets = async (daoMetadata: GardenMetadataType) => {
   try {
     if (daoMetadata.logo) {
       await createFileContent(
