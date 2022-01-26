c35cc6f9e09aef21ef8b56e4a8c3757139c2c515
Felipe Novaes Rocha <windholyghost@gmail.com>
2022-01-26 03:12:44 -0300

    feat: configured on yarn start based at env example/template

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Gardens UI  <a href="https://gardens.1hive.org/"><img align="right" src=".github/assets/gardens.svg" height="80px" /></a>
+# Gardens UI <a href="https://gardens.1hive.org/"><img align="right" src=".github/assets/gardens.svg" height="80px" /></a>
 
 ## Gardens are secure digital economies run by the community members that make them valuable.
 
@@ -7,31 +7,24 @@
 ## How to run locally
 
 #### Clone the repository
+
 ```
 git clone https://github.com/1Hive/gardens-ui.git
 ```
 
 #### Install dependencies
+
 ```
 cd gardens-ui
 yarn
 ```
 
 #### Start UI
+
 ```
 yarn start
 ```
 
 ### Environment variables
-```
-REACT_APP_GITHUB_API_TOKEN # API token for GitHub used to fetch Gardens metadata.
-REACT_APP_PINATA_API_TOKEN # API token for Pinata used to pin IPFS content.
-REACT_APP_POLYGON_ETH_NODE # Env to set a custom node on Polygon.
-REACT_APP_XDAI_ETH_NODE # Env to set a custom node on xDai.
-REACT_APP_RINKEBY_ETH_NODE # Env to set a custom node on Rinkeby.
-REACT_APP_PORTIS_ID # App ID to setup Portis provider.
-REACT_APP_POCKET_API_KEY # API key for Pocket to setup a ethers default provider connected to multiple nodes.
-REACT_APP_ALCHEMY_API_KEY # API key for Alchemy to setup a ethers default provider connected to multiple nodes.
-REACT_APP_ETHERSCAN_API_KEY # API key for Etherscan to setup a ethers default provider connected to multiple nodes.
-REACT_APP_INFURA_API_KEY # API key for Infura to setup a ethers default provider connected to multiple nodes.
-```
+
+Check out the [.env.example file](.env.example)
---
+++ scripts/check_env.ts
@@ -0,0 +1,76 @@
+import { exit } from 'process'
+import { readSingle, EnvVars } from './read-env-file'
+
+const ENV_PATH = './.env'
+const ENV_TEMPLATE_PATH = './.env.example'
+
+const THROW_ERROR_REQUIRED = true
+
+async function check_env() {
+  let env: EnvVars | undefined
+  let envTemplate: EnvVars | undefined
+  try {
+    envTemplate = await readSingle(ENV_TEMPLATE_PATH)
+  } catch (error) {
+    console.log(`'${ENV_TEMPLATE_PATH}' path not found`)
+  }
+  try {
+    env = await readSingle(ENV_PATH) // Attempts to read from ./.env
+  } catch (error) {
+    console.log(`'${ENV_PATH}' path not found`)
+  }
+  if (envTemplate) {
+    const keysNotOptionals = extractNotOptionals(envTemplate)
+
+    console.log(keysNotOptionals)
+    let isFoundRequired = false
+
+    if (env) {
+      const arrKeys = Object.keys(keysNotOptionals)
+      for (const keyNotOptional of arrKeys) {
+        for (const envKey of Object.keys(env)) {
+          if (envKey === keyNotOptional) {
+            isFoundRequired = true
+            console.log(
+              `WARNING: Missing Env ${envKey} it's defined not optional (required) in '${ENV_TEMPLATE_PATH}', create it on '${ENV_PATH}' and run it again`
+            )
+          }
+        }
+      }
+    } else {
+      console.log(
+        `First clone '${ENV_TEMPLATE_PATH}' to '${ENV_PATH}' and run it again`
+      )
+    }
+
+    if (isFoundRequired && THROW_ERROR_REQUIRED) {
+      throw new Error('Missing Required Envs, check the logs')
+    }
+  } else {
+    throw new Error(
+      `Something goes wrong. Env template in the path '${ENV_TEMPLATE_PATH}' not found`
+    )
+  }
+}
+
+function extractNotOptionals(envTemplate: EnvVars) {
+  let keysNotOptionals = {} as Record<string, string | undefined>
+
+  for (const keyTemplate of Object.keys(envTemplate)) {
+    const valueTemplate = envTemplate[keyTemplate]
+
+    if (valueTemplate && valueTemplate !== '')
+      keysNotOptionals = {
+        ...keysNotOptionals,
+        [keyTemplate]: envTemplate[keyTemplate],
+      }
+  }
+  return keysNotOptionals
+}
+
+check_env()
+  .then(console.log)
+  .catch((err) => {
+    console.error(err)
+    exit(1)
+  })
