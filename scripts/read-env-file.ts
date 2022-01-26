5dcb71ca690acb63889d0c9c819926178701982d
Felipe Novaes Rocha <windholyghost@gmail.com>
2022-01-26 03:04:39 -0300

    feat: read env file modified

---
+++ scripts/read-env-file.ts
@@ -0,0 +1,160 @@
+import { readFileSync, readFile } from 'fs'
+import { isAbsolute, resolve } from 'path'
+
+export interface EnvVars {
+  [key: string]: string | undefined
+}
+
+function mapToObject(map: Map<string, string>) {
+  const arr = [...map.entries()]
+  return arr.reduce((carrier: any, entry) => {
+    const [k, v] = entry
+    carrier[k] = v
+    return carrier
+  }, {})
+}
+
+function isValueQuoted(value: string): boolean {
+  return ["'", '"', '`'].some((quote) => {
+    return value.startsWith(quote) && value.endsWith(quote)
+  })
+}
+
+function stripSurroundingQuotes(value: string): string {
+  let result = value
+  if (isValueQuoted(value)) {
+    result = value.slice(1, value.length - 1)
+  }
+  return result
+}
+
+function normalizePath(path?: string): string {
+  path = path || resolve(process.cwd(), '.env')
+  return isAbsolute(path) ? path : resolve(process.cwd(), path)
+}
+
+function checkInvalidFormat(key: string, value: string, index: number) {
+  const undef = !key
+  const quoted = isValueQuoted(value)
+
+  const invalidSpace = /\s/.test(key) || (!quoted && /\s/.test(value))
+  const multiAssign = !quoted && value.split('=').length > 1
+
+  /* No key, No value, Key OR Value contain a space */
+  if (undef || invalidSpace || multiAssign) {
+    let reason
+    if (undef) reason = `${key ? 'value' : 'key'} undefined`
+    else if (invalidSpace) reason = 'invalid spacing'
+    else if (multiAssign) reason = 'multiple assignment operators'
+    throw new Error(`FORMAT:${reason}:${index + 1}`)
+  }
+}
+
+function makeVarsMap(lines: string[]) {
+  const map = new Map()
+  lines.forEach((line, index) => {
+    line = line.trim()
+    /* Skip empty and commented lines, preserving index for line count */
+    if (line.length === 0 || line[0] === '#') return
+
+    const splitLine = line.split('=')
+    const key = (splitLine[0] || '').trim()
+    const value = (splitLine.slice(1).join('=') || '').trim()
+
+    /* Check some error conditions and throw if met */
+    checkInvalidFormat(key, value, index)
+
+    /* If value was quoted, the asigned value should not invlude the quotes */
+    const mapValue = stripSurroundingQuotes(value)
+
+    map.set(key, mapValue)
+  })
+  return map
+}
+
+function processContent(path: string, content: string) {
+  try {
+    const lines = content.split('\n')
+    const map = makeVarsMap(lines)
+    return mapToObject(map)
+  } catch (error) {
+    const message = (error as any)?.message || ''
+    let err
+    if (message.includes('FORMAT:')) {
+      const splitMsg = message.split(':').slice(1)
+      const reason = splitMsg[0]
+      const line = parseInt(splitMsg[1], 10)
+      err = new Error(`Invalid file format (${reason}) at ${path}:${line}`)
+    } else err = error
+    throw err
+  }
+}
+
+/**
+ * Given a path to a file in .env format, returns key/value pairs in an object.
+ * @param {string} [path] The file path to read. If omitted, attempts to read from `./.env`
+ * @returns {Promise<EnvVars>} An object of corresponding key/value pairs
+ * @throws Error if file is not in valid format, or file is not found
+ */
+export async function readSingle(path?: string): Promise<EnvVars> {
+  const file = normalizePath(path)
+  return new Promise((resolve, reject) => {
+    readFile(file, { encoding: 'utf8' }, (err, content) => {
+      if (err) reject(err)
+      try {
+        resolve(processContent(file, content))
+      } catch (error) {
+        reject(error)
+      }
+    })
+  })
+}
+
+/**
+ * Given a path to a file in .env format, returns key/value pairs in an object.
+ * @param {string} [path] The file path to read. If omitted, attempts to read from `./.env`
+ * @returns {EnvVars} An object of corresponding key/value pairs
+ * @throws Error if file is not in valid format, or file is not found
+ */
+readSingle.sync = (path?: string): EnvVars => {
+  const file = normalizePath(path)
+  const content = readFileSync(file, 'utf8')
+  return processContent(file, content)
+}
+
+/**
+ * @description Given an array of paths, will read each and add the key/value
+ * pairs to an object which is returned at the end. Any re-used keys will be overwritten by
+ * the most recent value.
+ * @param {string[]} paths An array of file paths to read.
+ * @returns {Promise<EnvVars>} An aggregate object of key/value pairs
+ * @throws Error if any file is not in valid format or not found
+ */
+export async function readMultiple(paths: string[]): Promise<EnvVars> {
+  const readPromises = paths.map((p) => readSingle(p))
+  const varObjects = await Promise.all(readPromises)
+  return varObjects.reduce((carrier: any, varObject) => {
+    return {
+      ...carrier,
+      ...varObject,
+    }
+  }, {})
+}
+
+/**
+ * @description Given an array of paths, will read each and add the key/value
+ * pairs to an object which is returned at the end. Any re-used keys will be overwritten by
+ * the most recent value.
+ * @param {string[]} paths An array of file paths to read.
+ * @returns {EnvVars} An aggregate object of key/value pairs
+ * @throws Error if any file is not in valid format or not found
+ */
+readMultiple.sync = (paths: string[]): EnvVars => {
+  return paths.reduce((carrier: any, path) => {
+    const obj = readSingle.sync(path)
+    return {
+      ...carrier,
+      ...obj,
+    }
+  }, {})
+}
