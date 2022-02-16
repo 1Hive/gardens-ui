aac7773f7a1624ed6731a5bcf9a168c55ba8dc7a
Tiago Moutinho <tiago.moutinho@olx.com>
2022-02-16 21:26:56 +0000

    Improvements on mobile

--- src/components/Account/Buttons/HeaderButtons.tsx
+++ src/components/Account/Buttons/HeaderButtons.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
-import { Button, ButtonBase, IconConnect, IconCanvas } from '@1hive/1hive-ui'
-import { switchNetwork } from '@/networks'
+import React, { useCallback } from 'react'
+import {
+  Button,
+  IconConnect,
+  IconCanvas,
+  useTheme,
+  DropDown,
+} from '@1hive/1hive-ui'
+import { SUPPORTED_CHAINS, switchNetwork } from '@/networks'
 
 import useSupportedChain from '@/hooks/useSupportedChain'
 import AccountConnectedButton from './ConnectedButton'
+import { useWallet } from '@/providers/Wallet'
+import { getNetworkName } from '@/utils/web3-utils'
+import styled from 'styled-components'
 
 type WrongNetworkButton = {
   compact: any
@@ -19,30 +28,46 @@ const WrongNetworkButton = ({ compact }: WrongNetworkButton) => {
       }}
     >
       <Button
-        icon={<IconCanvas />}
-        label="Switch wallet to xDai"
         onClick={async () => await switchNetwork(100)}
-        display={compact ? 'icon' : 'all'}
-        style={{
-          boxShadow: 'none',
-        }}
-      />
-      <ButtonBase
         css={`
-          background-color: rgb(255, 104, 113);
-          border: 1px solid rgb(255, 104, 113);
-          color: #fff;
-          position: relative;
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          padding: 0 24px 0 16px;
-          width: 100%;
-          height: 40px;
+          overflow: hidden;
+          box-shadow: none;
+          padding-right: 0;
+          padding-left: 0;
         `}
       >
-        Wrong Network
-      </ButtonBase>
+        <div
+          style={{
+            justifyContent: 'center',
+            alignItems: 'center',
+            display: 'flex',
+            paddingRight: '16px',
+            paddingLeft: '16px',
+          }}
+        >
+          <IconCanvas />
+          Switch wallet to xDai
+        </div>
+
+        {!compact ? (
+          <div
+            css={`
+              background-color: rgb(255, 104, 113);
+              border: 1px solid rgb(255, 104, 113);
+              color: #fff;
+              position: relative;
+              display: flex;
+              align-items: center;
+              justify-content: center;
+              padding: 0 24px 0 16px;
+              width: 100%;
+              height: 40px;
+            `}
+          >
+            Wrong Network
+          </div>
+        ) : null}
+      </Button>
     </div>
   )
 }
@@ -53,20 +78,67 @@ type HeaderButtonProps = {
   compact: any
 }
 
-const HeaderButtons = ({ screenId, toggle, compact }: HeaderButtonProps) => {
-  const isSupportedNetwork = useSupportedChain()
+const ProviderDropdown = styled(DropDown)`
+  height: 30px;
+  padding-left: 12px;
+  border-radius: 6px;
 
-  return screenId === 'connected' ? (
-    <AccountConnectedButton onClick={toggle} />
-  ) : isSupportedNetwork === false ? (
-    <WrongNetworkButton compact={compact} />
-  ) : (
-    <Button
-      icon={<IconConnect />}
-      label="Enable account"
-      onClick={toggle}
-      display={compact ? 'icon' : 'all'}
-    />
+  > svg {
+    color: #000;
+  }
+`
+
+const HeaderButtons = ({ screenId, toggle, compact }: HeaderButtonProps) => {
+  const theme = useTheme()
+  const isSupportedNetwork = useSupportedChain()
+  const { preferredNetwork, onPreferredNetworkChange } = useWallet()
+
+  const supportedChains = SUPPORTED_CHAINS.map((chain) => getNetworkName(chain))
+  const selectedIndex = SUPPORTED_CHAINS.indexOf(preferredNetwork)
+
+  const handleNetworkChange = useCallback(
+    (index) => {
+      const chainId = SUPPORTED_CHAINS[index]
+      onPreferredNetworkChange(chainId)
+    },
+    [onPreferredNetworkChange]
+  )
+
+  return (
+    <div
+      style={{
+        display: 'grid',
+        gridTemplateColumns: 'auto auto',
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: '8px',
+      }}
+    >
+      {screenId === 'connected' ? (
+        <AccountConnectedButton onClick={toggle} />
+      ) : isSupportedNetwork === false ? (
+        <WrongNetworkButton compact={compact} />
+      ) : (
+        <Button
+          icon={<IconConnect />}
+          label="Enable account"
+          onClick={toggle}
+          display={compact ? 'icon' : 'all'}
+        />
+      )}
+
+      {isSupportedNetwork ? (
+        <ProviderDropdown
+          items={supportedChains}
+          onChange={handleNetworkChange}
+          selected={selectedIndex}
+          wide
+          style={{
+            background: theme.accent,
+          }}
+        />
+      ) : null}
+    </div>
   )
 }
 
