5f8f7e5e21a6448f47894519c7434c3577ab6e85
Tiago Moutinho <tiago.moutinho@olx.com>
2022-02-26 18:26:50 +0000

    remove test

--- src/components/Account/Buttons/ChangeNetworkModal.tsx
+++ src/components/Account/Buttons/ChangeNetworkModal.tsx
@@ -23,8 +23,6 @@ export default function ChangeNetworkModal({
   const { width } = useViewport()
 
   const handleSwitch = async () => {
-    throw new Error(`AA`)
-
     try {
       await switchNetwork(100)
       onClose()
