37082ec241969706b6d3cda77184de65d708f06b
0xGabi <gabrielpk.18@gmail.com>
2022-08-23 22:38:56 -0300

    fix: include more dark-mode theme colors and fix onboarding wizard

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -15,57 +15,88 @@ export default {
     challengeSurface: '#FFFDFA',
   },
   dark: {
-    _name: 'gardens',
+    _name: 'dark',
     _appearance: 'dark',
 
     background: '#031A3A',
     border: '#063374',
-    surface: '#041F46',
+    overlay: '#02142E',
     content: '#DEE2E7',
     contentSecondary: '#ACB5C2',
 
-    surfaceInteractive: '#05295D',
+    surface: '#041F46',
     surfaceContent: '#DFE3EA',
     surfaceContentSecondary: '#ACB5C2',
     surfaceIcon: '#DFE3EA',
     surfaceUnder: '#031A3A',
-    surfaceBar: '#51709E',
+    surfaceOpened: '#7c99d6',
+    surfaceSelected: '#405071',
+    surfaceHighlight: '#405071',
+    surfacePressed: '#3f4e6d',
+    surfaceInteractive: '#05295D',
 
-    link: '#7CA8FF',
-
-    badge: '#1F4782',
-    badgeContent: '#DEE2E7',
-
-    disabled: '#02142E',
-    disabledContent: '#818A97',
-    disabledBorder: '#1F4782',
-
-    help: '#DEE2E7',
-    helpIcon: '#88A8E4',
-    helpSurface: '#1F4782',
-    helpContent: '#031A3A',
+    feedbackSurface: '#4c5d82',
+    feedbackSurfaceContent: '#212B36',
+    feedbackSurfaceContentSecondary: '#637381',
 
     warning: '#FFA410',
     warningSurface: '#363A3B',
     warningSurfaceContent: '#FFA410',
 
-    info: '#00D2FF',
+    info: '#18D4FB',
     infoSurface: '#08436A',
     infoSurfaceContent: '#18D4FB',
+
+    help: '#DEE2E7',
+    helpIcon: '#88A8E4',
+    helpSurface: '#1F4782',
+    helpContent: '#031A3A',
+    helpSurfaceContent: '#212B36',
 
     negative: '#FF6D6D',
     negativeContent: '#FF6D6D',
     negativeSurface: '#1a0c0c',
     negativeSurfaceContent: '#FF6D6D',
 
-    accent: '#7FDBBD',
-    accentStart: '#64B7B4',
-    accentEnd: '#7FDBBD',
-    accentContent: '#13334b',
-
     positive: '#2BC58E',
     positiveContent: '#2BC58E',
     positiveSurface: '#0C4054',
+    positiveSurfaceContent: '#2cc68f',
+
+    badge: '#1F4782',
+    badgeContent: '#DEE2E7',
+    badgePressed: '#4c5d82',
+
+    tagIdentifier: '#564090',
+    tagIdentifierContent: '#b6a1ff',
+
+    tagNew: '#35585e',
+    tagNewContent: '#2cc68f',
+
+    tagIndicator: '#415279',
+    tagIndicatorContent: '#00d3ff',
+
+    tagActivity: '#00d2ff',
+    tagActivityContent: '#FFFFFF',
+
+    hint: '#6683c3',
+    link: '#7CA8FF',
+    focus: '#0ca5ff',
+    selected: '#08BEE5',
+    selectedContent: '#FFFFFF',
+    selectedDisabled: '#212B36',
+
+    disabled: '#02142E',
+    disabledContent: '#818A97',
+    disabledBorder: '#1F4782',
+    disabledIcon: '#6683c3',
+
+    control: '#042451',
+    controlBorder: '#2c3a58',
+    controlBorderPressed: '#65779a',
+    controlDisabled: '#3f4e6f',
+    controlSurface: '#F9FAFC',
+    controlUnder: '#F1F3F7',
 
     celeste: '#B19AFF',
     celesteBorder: '#B19AFF',
@@ -75,6 +106,19 @@ export default {
     challengeBorder: '#B3730B',
     challengeSurface: '#190f03',
 
+    accent: '#7FDBBD',
+    accentStart: '#64B7B4',
+    accentEnd: '#7FDBBD',
+    accentContent: '#13334b',
+
+    floating: '#1c2539',
+    floatingContent: '#FFFFFF',
+
     green: '#71EEB8',
+    yellow: '#F7D858',
+    red: '#F08658',
+    blue: '#3E7BF6',
+    brown: '#876559',
+    purple: '#7C80F2',
   },
 }
